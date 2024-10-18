9c3e52a16d47ee1262e352bb44eadd4061a37a81
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-10-18 11:39:37 -0400

    Update epg.sh

--- epg/epg.sh
+++ epg/epg.sh
@@ -4,11 +4,27 @@ cd /home/runner/work/strm/strm/iptv-org-epg && npm install
 
 # AD EPG
 
-npm run grab -- --site=andorradifusio.ad --output=../epg/epg-ad.xml --days=3 --maxConnections=2
+#npm run grab -- --site=andorradifusio.ad --output=../epg/epg-ad.xml --days=3 --maxConnections=2
+
+# BEIN EPG
+
+npm run grab -- --channels=../epg/bein.channels.xml --output=../epg/epg-bein.xml --days=3 --maxConnections=10
+
+# FO EPG
+
+npm run grab -- --site=kvf.fo --output=../epg/epg-fo.xml --days=3 --maxConnections=2
 
 # IS EPG
 
-npm run grab -- --channels=../epg/is.channels.xml --output=../epg/epg-is.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/is.channels.xml --output=../epg/epg-is.xml --days=3 --maxConnections=10
+
+# MT EPG
+
+npm run grab -- --channels=../epg/mt.channels.xml --output=../epg/epg-mt.xml --days=3 --maxConnections=10
+
+# NZ EPG
+
+npm run grab -- --channels=../epg/nz.channels.xml --output=../epg/epg-nz.xml --days=3 --maxConnections=10
 
 # PT EPG
 
