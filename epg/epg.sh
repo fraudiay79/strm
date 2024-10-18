d5642255449704c86cea42821f9cb3c7939365b4
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-10-18 16:52:32 -0400

    Update epg.sh

--- epg/epg.sh
+++ epg/epg.sh
@@ -20,39 +20,39 @@ npm run grab -- --channels=../epg/bein.channels.xml --output=../epg/epg-bein.xml
 
 # CZ EPG
 
-npm run grab -- --site=mujtvprogram.cz --output=../epg/epg-cz.xml --days=3 --maxConnections=10
+#npm run grab -- --site=mujtvprogram.cz --output=../epg/epg-cz.xml --days=3 --maxConnections=10
 
 # DE EPG
 
-npm run grab -- --site=web.magentatv.de --output=../epg/epg-de.xml --days=3 --maxConnections=10
+#npm run grab -- --site=web.magentatv.de --output=../epg/epg-de.xml --days=3 --maxConnections=10
 
 # DK EPG
 
-npm run grab -- --site=allente.dk --output=../epg/epg-dk.xml --days=3 --maxConnections=10
+#npm run grab -- --site=allente.dk --output=../epg/epg-dk.xml --days=3 --maxConnections=10
 
 # FR EPG
 
-npm run grab -- --channels=../epg/fr.channels.xml --output=../epg/epg-fr.xml --days=3 --maxConnections=10
+#npm run grab -- --site=programme-tv.net --output=../epg/epg-fr.xml --days=3 --maxConnections=10
 
 # FI EPG
 
-npm run grab -- --channels=../epg/fi.channels.xml --output=../epg/epg-fi.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/fi.channels.xml --output=../epg/epg-fi.xml --days=3 --maxConnections=10
 
 # FO EPG
 
-npm run grab -- --site=kvf.fo --output=../epg/epg-fo.xml --days=3 --maxConnections=2
+#npm run grab -- --site=kvf.fo --output=../epg/epg-fo.xml --days=3 --maxConnections=2
 
 # GE EPG
 
-npm run grab -- --site=magticom.ge --output=../epg/epg-ge.xml --days=3 --maxConnections=10
+#npm run grab -- --site=magticom.ge --output=../epg/epg-ge.xml --days=3 --maxConnections=10
 
 # GR EPG
 
-npm run grab -- --channels=../epg/gr.channels.xml --output=../epg/epg-gr.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/gr.channels.xml --output=../epg/epg-gr.xml --days=3 --maxConnections=10
 
 # HU EPG
 
-npm run grab -- --site=musor.tv --output=../epg/epg-hu.xml --days=3 --maxConnections=10
+#npm run grab -- --site=musor.tv --output=../epg/epg-hu.xml --days=3 --maxConnections=10
 
 # IE EPG
 
@@ -60,39 +60,39 @@ npm run grab -- --channels=../epg/ie.channels.xml --output=../epg/epg-ie.xml --d
 
 # IN EPG
 
-npm run grab -- --site=dishtv.in --output=../epg/epg-in.xml --days=3 --maxConnections=2
+#npm run grab -- --site=dishtv.in --output=../epg/epg-in.xml --days=3 --maxConnections=2
 
 # IS EPG
 
-npm run grab -- --channels=../epg/is.channels.xml --output=../epg/epg-is.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/is.channels.xml --output=../epg/epg-is.xml --days=3 --maxConnections=10
 
 # IT EPG
 
-npm run grab -- --site=superguidatv.it --output=../epg/epg-it.xml --days=3 --maxConnections=2
+#npm run grab -- --site=superguidatv.it --output=../epg/epg-it.xml --days=3 --maxConnections=2
 
 # JP EPG
 
-npm run grab -- --site=tvguide.myjcom.jp --output=../epg/epg-jp.xml --days=3 --maxConnections=2
+#npm run grab -- --site=tvguide.myjcom.jp --output=../epg/epg-jp.xml --days=3 --maxConnections=2
 
 # MK EPG
 
-npm run grab -- --site=maxtvgo.mk --output=../epg/epg-mk.xml --days=3 --maxConnections=10
+#npm run grab -- --site=maxtvgo.mk --output=../epg/epg-mk.xml --days=3 --maxConnections=10
 
 # MN EPG
 
-npm run grab -- --site=zuragt.mn --output=../epg/epg-mn.xml --days=3 --maxConnections=2
+#npm run grab -- --site=zuragt.mn --output=../epg/epg-mn.xml --days=3 --maxConnections=2
 
 # MT EPG
 
-npm run grab -- --channels=../epg/mt.channels.xml --output=../epg/epg-mt.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/mt.channels.xml --output=../epg/epg-mt.xml --days=3 --maxConnections=10
 
 # NL EPG
 
-npm run grab -- --site=ziggogo.tv --output=../epg/epg-nl.xml --days=3 --maxConnections=10
+#npm run grab -- --site=ziggogo.tv --output=../epg/epg-nl.xml --days=3 --maxConnections=10
 
 # NO EPG
 
-npm run grab -- --site=allente.no --output=../epg/epg-no.xml --days=3 --maxConnections=10
+#npm run grab -- --site=allente.no --output=../epg/epg-no.xml --days=3 --maxConnections=10
 
 # NZ EPG
 
@@ -100,43 +100,43 @@ npm run grab -- --channels=../epg/nz.channels.xml --output=../epg/epg-nz.xml --d
 
 # PL EPG
 
-npm run grab -- --site=programtv.onet.pl --output=../epg/epg-pl.xml --days=3 --maxConnections=10
+#npm run grab -- --site=programtv.onet.pl --output=../epg/epg-pl.xml --days=3 --maxConnections=10
 
 # PT EPG
 
-npm run grab -- --channels=../epg/pt.channels.xml --output=../epg/epg-pt.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/pt.channels.xml --output=../epg/epg-pt.xml --days=3 --maxConnections=10
 
 # RO EPG
 
-npm run grab -- --site=programetv.ro --output=../epg/epg-ro.xml --days=3 --maxConnections=10
+#npm run grab -- --site=programetv.ro --output=../epg/epg-ro.xml --days=3 --maxConnections=10
 
 # RS EPG
 
-npm run grab -- --site=mts.rs --output=../epg/epg-rs.xml --days=3 --maxConnections=10
+#npm run grab -- --site=mts.rs --output=../epg/epg-rs.xml --days=3 --maxConnections=10
 
 # SE EPG
 
-npm run grab -- --site=allente.se --output=../epg/epg-se.xml --days=3 --maxConnections=10
+#npm run grab -- --site=allente.se --output=../epg/epg-se.xml --days=3 --maxConnections=10
 
 # SG EPG
 
-npm run grab -- --site=mewatch.sg --output=../epg/epg-sg.xml --days=3 --maxConnections=10
+#npm run grab -- --site=mewatch.sg --output=../epg/epg-sg.xml --days=3 --maxConnections=10
 
 # TR EPG
 
-npm run grab -- --channels=../epg/tr.channels.xml --output=../epg/epg-tr.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/tr.channels.xml --output=../epg/epg-tr.xml --days=3 --maxConnections=10
 
 # UK EPG
 
-npm run grab -- --site=mytelly.co.uk --output=../epg/epg-uk.xml --days=3 --maxConnections=10
+#npm run grab -- --site=mytelly.co.uk --output=../epg/epg-uk.xml --days=3 --maxConnections=10
 
 # US EPG
 
-npm run grab -- --channels=../epg/us.channels.xml --output=../epg/epg-us.xml --days=3 --maxConnections=10
+#npm run grab -- --channels=../epg/us.channels.xml --output=../epg/epg-us.xml --days=3 --maxConnections=10
 
 # UY EPG
 
-npm run grab -- --site=programacion.tcc.com.uy --output=../epg/epg-uy.xml --days=3 --maxConnections=10
+#npm run grab -- --site=programacion.tcc.com.uy --output=../epg/epg-uy.xml --days=3 --maxConnections=10
 
 # ZA EPG
 
