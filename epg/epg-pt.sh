04d4dbb0d96ac5b7524ea6a421c2510ccee0c16f
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-10-18 08:05:03 -0400

    Create epg-pt.sh

---
+++ epg/epg-pt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Rytec EPG
+
+cd ../epg
+
+wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
+
+# Compress EPG xml files
+
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+
+# Remove EPG xml files
+
+rm epg*.xml
+
+exit 0
