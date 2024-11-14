4f0188d6f837d3582c34e05acde97567ce9f0993
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-14 16:22:53 -0500

    Create epg_main.sh

---
+++ epg/epg_main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/runner/work/strm/strm/fraudiay79-epg && npm install
+
+# EPG
+
+#npm run grab -- --channels=../epg/channels/test.channels.xml --output=../epg/epg-test.xml --days=2 --maxConnections=20
+npm run grab -- --channels=../epg/channels/channels.xml --output=../epg/epg-channels.xml --days=2 --maxConnections=20 --timeout=90000
+
+# Compress EPG xml files
+cd ../epg/
+
+gzip -k -f -9 epg*.xml
+
+# Remove EPG xml files
+
+rm epg*.xml
+
+exit 0
