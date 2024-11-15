8186b7a3d51e53ff58d1c41c114b785bbb4ff503
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-15 12:32:19 -0500

    Update epg_main.sh

--- epg/epg_main.sh
+++ epg/epg_main.sh
@@ -4,7 +4,6 @@ cd /home/runner/work/strm/strm/fraudiay79-epg && npm install
 
 # EPG
 
-#npm run grab -- --channels=../epg/channels/test.channels.xml --output=../epg/epg-test.xml --days=2 --maxConnections=20
 npm run grab -- --channels=../epg/channels/channels.xml --output=../epg/epg-channels.xml --days=2 --maxConnections=20 --timeout=90000
 
 # Compress EPG xml files
