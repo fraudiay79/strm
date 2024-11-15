e98e7e5e7661155373fbf0fef59fb6e9a6ede714
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-11-15 18:02:32 -0500

    Update epg_main.sh

--- epg/epg_main.sh
+++ epg/epg_main.sh
@@ -4,9 +4,6 @@ cd /home/runner/work/strm/strm/fraudiay79-epg && npm install
 
 # EPG
 
-#npm run grab -- --site=starhubtvplus.com --output=../epg/epg-hk.xml --days=2 --maxConnections=20 --timeout=90000
-#npm run grab -- --site=dishtv.in --output=../epg/epg-in2.xml --days=2 --maxConnections=20 --timeout=90000
-#npm run grab -- --site=watch.tataplay.com --output=../epg/epg-in1.xml --days=2 --maxConnections=20 --timeout=90000
 npm run grab -- --channels=../epg/channels/channels.xml --output=../epg/epg-channels.xml --days=2 --maxConnections=20 --timeout=90000
 
 # Compress EPG xml files
