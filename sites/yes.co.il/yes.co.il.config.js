72ede860fd676fb4c8ae6acd142818d4140ebc33
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-23 21:05:44 -0500

    Update yes.co.il.config.js

--- sites/yes.co.il/yes.co.il.config.js
+++ sites/yes.co.il/yes.co.il.config.js
@@ -2,87 +2,77 @@ const axios = require('axios');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
+const cheerio = require('cheerio');
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
 
 module.exports = {
   site: 'yes.co.il',
-  days: 3,
+  timezone: 'UTC',
+  maxdays: 7,
+  cultureinfo: 'he-IL',
+  charset: 'utf-8',
+  titlematchfactor: 90,
+  episodesystem: 'onscreen',
   request: {
     cache: {
       ttl: 60 * 60 * 1000 // 1 hour
+    },
+    headers: {
+      'Accept-Language': 'he-IL',
+      'Origin': 'https://www.yes.co.il',
+      'Accept-Encoding': 'gzip, deflate, br'
     }
   },
+  urldateFormat: 'YYYY-MM-DD',
   url({ channel, date }) {
-    return `https://www.yes.co.il/o/yes/servletlinearsched/getscheduale?startdate=${date.format('YYYYMMDD')}&p_auth=${channel.p_auth}`;
+    const formattedDate = dayjs(date).format(this.urldateFormat);
+    return `https://svc.yes.co.il/api/content/broadcast-schedule/channels/${channel.site_id}?date=${formattedDate}&ignorePastItems=true`;
   },
   async parser({ content }) {
-    const shows = [];
     const data = JSON.parse(content);
+    const programs = [];
+    
+    data.items.forEach(item => {
+      const start = dayjs(item.starts).toISOString();
+      const stop = dayjs(item.ends).toISOString();
+      const title = item.title;
+      const description = item.description;
+      const icon = item.imageUrl;
 
-    data.forEach(program => {
-      const show = {
-        channel: program.channelID,
-        title: program.scheduleItemName,
-        description: program.scheduleItemSynopsis || 'No description available',
-        start: dayjs(program.startDate).utc().format(),
-        stop: dayjs(program.startDate).add(dayjs.duration(program.broadcastItemDuration)).utc().format()
-      };
-      shows.push(show);
+      programs.push({
+        start,
+        stop,
+        title,
+        description,
+        icon
+      });
     });
 
-    return shows;
-  },
-  async getAuthToken() {
-    try {
-      const url = 'https://www.yes.co.il/content/tvguide';
-      const response = await axios.get(url);
-      const textToSearch = ';Liferay.authToken=';
-      const mainPageHtml = response.data;
-      const idx = mainPageHtml.indexOf(textToSearch);
-      if (idx === -1) {
-        throw new Error('Auth token not found');
-      }
-      const val = mainPageHtml.substring(idx + textToSearch.length);
-      const authToken = val.split(';')[0].trim();
-      return authToken;
-    } catch (error) {
-      console.error('Error fetching auth token:', error);
-      return null;
-    }
+    return programs;
   },
   async channels() {
-    const authToken = await this.getAuthToken();
-    if (!authToken) {
-      console.error('No auth token available');
-      return [];
-    }
-
-    const url = `https://www.yes.co.il/o/yes/servletlinearsched/getchannels?p_auth=${authToken}`;
-
-    try {
-      const response = await axios.get(url);
-
-      if (response.data && Array.isArray(response.data)) {
-        const channels = response.data.map(channel => {
-          return {
-            lang: 'he',
-            name: channel.channelName,
-            site_id: channel.channelID,
-            p_auth: authToken
-          };
-        });
-        return channels;
-      } else {
-        console.error('Response data is not an array:', response.data);
-        return [];
+    const response = await axios.get('https://svc.yes.co.il/api/content/broadcast-schedule/channels?page=0&pageSize=200', {
+      headers: {
+        'Accept-Language': 'he-IL',
+        'Origin': 'https://www.yes.co.il',
+        'Accept-Encoding': 'gzip, deflate, br'
       }
-    } catch (error) {
-      console.error('Error fetching channels:', error);
-      return [];
-    }
+    });
+
+    const data = response.data;
+    const channels = [];
+
+    data.items.forEach(item => {
+      channels.push({
+        lang: 'he',
+        name: item.channelName.replace('|', '’'),
+        site_id: item.channelId,
+        logo: `https://www.yes.co.il${item.imageUrl}`
+      });
+    });
+
+    return channels;
   }
 };
