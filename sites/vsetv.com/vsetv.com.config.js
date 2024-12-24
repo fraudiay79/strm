0bcfa2666ece129e819569e74bc340ed9919fde7
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-24 13:26:13 -0500

    Update vsetv.com.config.js

--- sites/vsetv.com/vsetv.com.config.js
+++ sites/vsetv.com/vsetv.com.config.js
@@ -27,39 +27,39 @@ module.exports = {
     const programs = []
     const formattedDate = dayjs(date).format('YYYY-MM-DD')
 
-	$('#schedule_container').each((index, container) => {
-		let previousEndTime = null;
+    $('#schedule_container').each((index, container) => {
+      let previousEndTime = null
 
-    $(container).children().each((i, elem) => {
-      if ($(elem).hasClass('time')) {
-        const timeString = parseTime($(elem).find('img, text'));
-        const startTime = dayjs.tz(`${formattedDate} ${timeString}`, 'YYYY-MM-DD HH:mm', 'Europe/Kiev').toISOString()
+      $(container).children().each((i, elem) => {
+        if ($(elem).hasClass('time')) {
+          const timeString = parseTime($(elem).contents())
+          const startTime = dayjs.tz(`${formattedDate} ${timeString}`, 'YYYY-MM-DD HH:mm', 'Europe/Kiev').toISOString()
 
-        if (previousEndTime) {
-          programs[programs.length - 1].stop = startTime
+          if (previousEndTime) {
+            programs[programs.length - 1].stop = startTime
+          }
+
+          programs.push({
+            title: '',
+            start: startTime,
+            stop: '' // Will be updated when the next program's start time is found
+          })
+
+          previousEndTime = startTime
+        } else if ($(elem).hasClass('prname2')) {
+          const title = $(elem).text().trim() || $(elem).find('a').text().trim()
+          programs[programs.length - 1].title = title
         }
+      })
 
-        programs.push({
-          title: title,
-          start: startTime,
-          stop: '' // Will be updated when the next program's start time is found
-        })
-
-        previousEndTime = startTime;
-      } else if ($(elem).hasClass('prname2')) {
-	    const title = $(elem).text().trim() || $(elem).find('a').text().trim()
-        programs[programs.length - 1].title = $(elem).text().trim()
+      // Set stop time for the last program in the container (assuming it ends after 1 hour)
+      if (previousEndTime) {
+        programs[programs.length - 1].stop = dayjs(previousEndTime).add(1, 'hour').toISOString()
       }
     })
 
-    // Set stop time for the last program in the container (assuming it ends after 1 hour)
-    if (previousEndTime) {
-      programs[programs.length - 1].stop = dayjs(previousEndTime).add(1, 'hour').toISOString()
-    }
-  })
-
-  return programs
-},
+    return programs
+  },
   async channels() {
     const response = await axios.get('http://www.vsetv.com/schedule.html', {
       headers: {
@@ -70,21 +70,21 @@ module.exports = {
     const channels = []
 
     $('select[name="selected_channel"] option').each((index, element) => {
-    const siteId = $(element).val()
-    const name = $(element).text().trim()
+      const siteId = $(element).val()
+      const name = $(element).text().trim()
 
-    // Exclude options with "package_no" value and empty options
-    if (siteId && siteId !== 'package_no' && name && name !== '--выберите каналы--' && name !== '--каналы по регионам--' && name !== '--каналы по операторам--' && name !== '--отдельные каналы--') {
-      channels.push({
-        lang: 'uk',
-        name: name,
-        site_id: siteId
-			})
-		}
-	})
+      // Exclude options with "package_no" value and empty options
+      if (siteId && siteId !== 'package_no' && name && name !== '--выберите каналы--' && name !== '--каналы по регионам--' && name !== '--каналы по операторам--' && name !== '--отдельные каналы--') {
+        channels.push({
+          lang: 'uk',
+          name: name,
+          site_id: siteId
+        })
+      }
+    })
 
-	return channels
-	}
+    return channels
+  }
 }
 
 function parseTime(timeHtml) {
@@ -92,7 +92,7 @@ function parseTime(timeHtml) {
   const mapping = {
     "/pic/j8.gif": "0",
     "/pic/p7.gif": "5"
-  };
+  }
 
   let timeString = ""
   timeHtml.each((index, elem) => {
