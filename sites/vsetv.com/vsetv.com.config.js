817d90724c79610557005e63a61ac2274a1774a6
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-24 13:54:00 -0500

    Update vsetv.com.config.js

--- sites/vsetv.com/vsetv.com.config.js
+++ sites/vsetv.com/vsetv.com.config.js
@@ -48,7 +48,7 @@ module.exports = {
 
           previousEndTime = startTime
         } else if ($(elem).hasClass('pastprname2')) {
-          const title = $(elem).text().match(/"[^"]*"/) ? $(elem).text().match(/"[^"]*"/)[0] : $(elem).text().trim()
+          const title = $(elem).text().match(/"[^"]*"/) ? $(elem).text().match(/"[^"]*"/)[0]
           programs[programs.length - 1].title = title
         }
       })
