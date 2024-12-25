4fe75a049ff91108c66505a2c88f972d95dd83a1
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-25 09:19:54 -0500

    Update ipko.tv.config.js

--- sites/ipko.tv/ipko.tv.config.js
+++ sites/ipko.tv/ipko.tv.config.js
@@ -1,23 +1,20 @@
-const axios = require('axios');
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
-const fetch = require('node-fetch');
-const { upperCase } = require('lodash');
+const axios = require('axios')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
 module.exports = {
   site: 'ipko.tv',
   timezone: 'Europe/Belgrade',
   days: 5,
+  url: 'https://stargate.ipko.tv/api/titan.tv.WebEpg/GetWebEpgData',
   request: {
-    cache: {
-      ttl: 60 * 60 * 1000 // 1 hour
-    },
+    method: 'POST',
     headers: {
       'Host': 'stargate.ipko.tv',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
@@ -32,50 +29,43 @@ module.exports = {
       'Sec-Fetch-Site': 'cross-site',
       'Sec-GPC': '1',
       'Connection': 'keep-alive'
+    },
+    data({ channel, date }) {
+      const params = new URLSearchParams()
+	  const todayEpoch = date.startOf('day').utc().valueOf()
+	  const nextDayEpoch = date.add(1, 'day').startOf('day').utc().valueOf()
+      params.append(
+        JSON.stringify({ch_ext_id: `${channel.site_id}`, from: `${todayEpoch}`, to: `${nextDayEpoch}`]
+        })
+      )
+
+      return params
     }
   },
-  url({ date, channel }) {
-    return {
-      url: "https://stargate.ipko.tv/api/titan.tv.WebEpg/GetWebEpgData",
-      data: JSON.stringify({
-        ch_ext_id: channel.site_id,
-        from: date.startOf('day').unix(),
-        to: date.endOf('day').unix()
-      })
-    };
-  },
-  async fetchEpg({ channel, date }) {
-    const { url, data } = this.url({ channel, date });
-    const response = await axios.post(url, data, {
-      headers: this.headers
-    });
-
-    return response.data;
-  },
   parser: function ({ content }) {
-    const programs = [];
+    const programs = []
 
-    const data = JSON.parse(content);
+    const data = JSON.parse(content)
     data.shows.forEach(show => {
-      const start = dayjs.unix(show.show_start).utc();
-      const stop = dayjs.unix(show.show_end).utc();
+      const start = dayjs.unix(show.show_start).utc()
+      const stop = dayjs.unix(show.show_end).utc()
       const programData = {
         title: show.title,
         description: show.summary || 'No description available',
         start: start.toISOString(),
         stop: stop.toISOString(),
         thumbnail: show.thumbnail
-      };
+      }
 
-      programs.push(programData);
-    });
+      programs.push(programData)
+    })
 
-    return programs;
+    return programs
   },
   async channels() {
     const response = await axios.post('https://stargate.ipko.tv/api/titan.tv.WebEpg/ZapList', JSON.stringify({ includeRadioStations: true }), {
       headers: this.request.headers
-    });
+    })
 
     const data = response.data.data;
 
@@ -84,6 +74,6 @@ module.exports = {
       name: String(item.channel.title),
       site_id: String(item.channel.id),
       logo: String(item.channel.logo)
-    }));
+    }))
   }
-};
+}
