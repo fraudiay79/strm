c20cd277eec4c847412ab98301a4b8a8a24be3fa
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-26 14:20:18 -0500

    Create neo.io.test.js

---
+++ sites/neo.io/neo.io.test.js
@@ -0,0 +1,121 @@
+const { parser, url } = require('./neo.io.config.js')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+dayjs.extend(utc)
+
+const date = dayjs.utc('2024-12-26', 'YYYY-MM-DD').startOf('day')
+const channel = {
+  site_id: 'tv-slo-1',
+  xmltv_id: 'TVSLO1.si'
+}
+
+it('can generate valid url', () => {
+  expect(url({ date, channel })).toBe('https://stargate.telekom.si/api/titan.tv.WebEpg/GetWebEpgData')
+})
+
+it('can parse response', () => {
+  const content = `
+  {
+    "shows": [
+        {
+            "title": "Napovedujemo",
+            "show_start": 1735185900,
+            "show_end": 1735192200,
+            "timestamp": "5:05 - 6:50",
+            "show_id": "CUP_IECOM_SLO1_10004660",
+            "thumbnail": "https://ngimg.siol.tv/sioltv/mtcmsprod/52/0/0/5200d01a-fe5f-487e-835a-274e77227a6b.jpg",
+            "is_adult": false,
+            "friendly_id": "napovedujemo_db48",
+            "pg": "",
+            "genres": [
+                "napovednik"
+            ],
+            "year": 0,
+            "summary": "Vabilo k ogledu na\u0161ih oddaj.",
+            "categories": "Ostalo",
+            "stb_only": false,
+            "is_live": false,
+            "original_title": "Napovedujemo"
+        },
+        {
+            "title": "S0E0 - Hrabri zaj\u010dki: Prvi sneg",
+            "show_start": 1735192200,
+            "show_end": 1735192800,
+            "timestamp": "6:50 - 7:00",
+            "show_id": "CUP_IECOM_SLO1_79637910",
+            "thumbnail": "https://ngimg.siol.tv/sioltv/mtcmsprod/d6/4/5/d6456f4a-4f0a-4825-90c1-1749abd59688.jpg",
+            "is_adult": false,
+            "friendly_id": "hrabri_zajcki_prvi_sneg_1619",
+            "pg": "",
+            "genres": [
+                "risanka"
+            ],
+            "year": 2020,
+            "summary": "Hrabri zaj\u010dki so prispeli v borov gozd in izkusili prvi sneg. Bob in Bu \u0161e nikoli nista videla snega. Mami kuha koren\u010dkov kakav, Bu in Bob pa kmalu spoznata novega prijatelja, losa Danija.",
+            "categories": "Otro\u0161ki/Mladinski",
+            "stb_only": false,
+            "is_live": false,
+            "original_title": "S0E0 - Brave Bunnies"
+        },
+        {
+            "title": "Dobro jutro",
+            "show_start": 1735192800,
+            "show_end": 1735203900,
+            "timestamp": "7:00 - 10:05",
+            "show_id": "CUP_IECOM_SLO1_79637911",
+            "thumbnail": "https://ngimg.siol.tv/sioltv/mtcmsprod/e1/2/d/e12d8eb4-693a-43d3-89d4-fd96dade9f0f.jpg",
+            "is_adult": false,
+            "friendly_id": "dobro_jutro_2f10",
+            "pg": "",
+            "genres": [
+                "zabavna oddaja"
+            ],
+            "year": 2024,
+            "summary": "Oddaja Dobro jutro poleg informativnih in zabavnih vsebin podaja koristne nasvete o najrazli\u010dnej\u0161ih tematikah iz vsakdanjega \u017eivljenja.",
+            "categories": "Razvedrilni program",
+            "stb_only": false,
+            "is_live": false,
+            "original_title": "Dobro jutro"
+        }
+    ]
+  }`
+
+  const result = parser({ content, channel }).map(p => {
+    p.start = p.start
+    p.stop = p.stop
+    return p
+  })
+
+  expect(result).toMatchObject([
+    {
+      title: "Napovedujemo",
+      description: "Vabilo k ogledu na\u0161ih oddaj.",
+      start: "2024-12-26T04:00:00.000Z",
+      stop: "2024-12-26T06:00:00.000Z",
+      thumbnail: "https://ngimg.siol.tv/sioltv/mtcmsprod/52/0/0/5200d01a-fe5f-487e-835a-274e77227a6b.jpg"
+    },
+    {
+      title: "S0E0 - Hrabri zaj\u010dki: Prvi sneg",
+      description: "Hrabri zaj\u010dki so prispeli v borov gozd in izkusili prvi sneg. Bob in Bu \u0161e nikoli nista videla snega. Mami kuha koren\u010dkov kakav, Bu in Bob pa kmalu spoznata novega prijatelja, losa Danija.",
+      start: "2024-12-26T06:00:00.000Z",
+      stop: "2024-12-26T08:00:00.000Z",
+      thumbnail: "https://ngimg.siol.tv/sioltv/mtcmsprod/d6/4/5/d6456f4a-4f0a-4825-90c1-1749abd59688.jpg"
+    },
+    {
+      title: "Dobro jutro",
+      description: "Oddaja Dobro jutro poleg informativnih in zabavnih vsebin podaja koristne nasvete o najrazli\u010dnej\u0161ih tematikah iz vsakdanjega \u017eivljenja.",
+      start: "2024-12-26T08:00:00.000Z",
+      stop: "2024-12-26T10:00:00.000Z",
+      thumbnail: "https://ngimg.siol.tv/sioltv/mtcmsprod/e1/2/d/e12d8eb4-693a-43d3-89d4-fd96dade9f0f.jpg"
+    }
+  ])
+})
+
+it('can handle empty guide', () => {
+  const result = parser({
+    content: '{"shows":[]}'
+  });
+  expect(result).toMatchObject([]);
+})
