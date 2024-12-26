09ac968fe7bbd140368d788d856cd8aba9aa5118
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-26 15:38:41 -0500

    Update chada.ma.test.js

--- sites/chada.ma/chada.ma.test.js
+++ sites/chada.ma/chada.ma.test.js
@@ -1,16 +1,16 @@
-const { parser, url } = require('./chada.ma.config.js'); // Adjust the path to your module
-const axios = require('axios');
-const dayjs = require('dayjs');
-const cheerio = require('cheerio');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
+const { parser, url } = require('./chada.ma.config.js')
+const axios = require('axios')
+const dayjs = require('dayjs')
+const cheerio = require('cheerio')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
-jest.mock('axios');
+jest.mock('axios')
 
 const mockHtmlContent = `
   <div class="pm0 col-md-8" id="stopfix">
@@ -30,17 +30,17 @@ const mockHtmlContent = `
 `;
 
 it('can generate valid url', () => {
-  expect(url()).toBe('https://chada.ma/fr/chada-tv/grille-tv/');
+  expect(url()).toBe('https://chada.ma/fr/chada-tv/grille-tv/')
 });
 
 it('can parse response', () => {
-  const content = mockHtmlContent;
+  const content = mockHtmlContent
 
   const result = parser({ content }).map(p => {
-    p.start = dayjs(p.start).tz('Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ');
-    p.stop = dayjs(p.stop).tz('Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ');
-    return p;
-  });
+    p.start = dayjs(p.start).tz('Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ')
+    p.stop = dayjs(p.stop).tz('Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ')
+    return p
+  })
 
   expect(result).toMatchObject([
     {
@@ -49,12 +49,12 @@ it('can parse response', () => {
       start: dayjs.tz('00:00', 'HH:mm', 'Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ'),
       stop: dayjs.tz('09:00', 'HH:mm', 'Africa/Casablanca').format('YYYY-MM-DDTHH:mm:ssZ')
     }
-  ]);
-});
+  ])
+})
 
 it('can handle empty guide', () => {
   const result = parser({
     content: '<div class="pm0 col-md-8" id="stopfix"><div class="posts-area"></div></div>'
-  });
-  expect(result).toMatchObject([]);
-});
+  })
+  expect(result).toMatchObject([])
+})
