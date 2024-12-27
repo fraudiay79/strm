deeea1517fca3e47cd9bde3b919e88d1cfde976a
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-27 15:25:50 -0500

    Update cosmotetv.gr.test.js

--- sites/cosmotetv.gr/cosmotetv.gr.test.js
+++ sites/cosmotetv.gr/cosmotetv.gr.test.js
@@ -1,17 +1,18 @@
-const { parser, url, channels } = require('.cosmotetv.gr.config.js'); // Adjust the path to your module
+const { parser, url, channels } = require('./cosmotetv.gr.config.js');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
 const timezone = require('dayjs/plugin/timezone');
 const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
 
 dayjs.extend(utc);
 dayjs.extend(customParseFormat);
 dayjs.extend(timezone);
 
 jest.mock('axios');
+
+const date = dayjs.utc('2024-12-26', 'YYYY-MM-DD').startOf('d');
+const channel = { site_id: 'vouli', xmltv_id: 'HellenicParliamentTV.gr' };
 
 const mockChannelData = {
   "channels": [
@@ -67,59 +68,41 @@ const mockEpgData = {
   ]
 };
 
-describe('cosmotetv.gr module', () => {
-  it('can generate valid url', () => {
-    const date = dayjs('2024-12-26');
-    const channel = { site_id: 'promohd' };
-    const generatedUrl = url({ date, channel });
-    expect(generatedUrl).toBe('https://mwapi-prod.cosmotetvott.gr/api/v3.4/epg/listings/el?from=1735250400&to=1735336799&callSigns=promohd&endingIncludedInRange=false');
+it('can generate valid url', () => {
+  const startOfDay = dayjs(date).startOf('day').utc().unix();
+  const endOfDay = dayjs(date).endOf('day').utc().unix();
+  expect(url({ date, channel })).toBe(`https://mwapi-prod.cosmotetvott.gr/api/v3.4/epg/listings/el?from=${startOfDay}&to=${endOfDay}&callSigns=${channel.site_id}&endingIncludedInRange=false`);
+});
+
+it('can parse response', () => {
+  const content = JSON.stringify(mockEpgData);
+  const result = parser({ date, content }).map(p => {
+    p.start = dayjs(p.start).toISOString();
+    p.stop = dayjs(p.stop).toISOString();
+    return p;
   });
 
-  it('can parse response', () => {
-    const content = JSON.stringify(mockEpgData);
-    const result = parser({ date: dayjs('2024-12-26'), content });
-    expect(result).toMatchObject([
-      {
-        title: "Sample Program 1",
-        description: "Description 1",
-        category: "Genre 1",
-        image: "https://example.com/image1.jpg",
-        start: "2024-12-26T22:00:00.000Z",
-        stop: "2024-12-26T23:00:00.000Z"
-      },
-      {
-        title: "Sample Program 2",
-        description: "Description 2",
-        category: "Genre 2",
-        image: "https://example.com/image2.jpg",
-        start: "2024-12-26T23:00:00.000Z",
-        stop: "2024-12-27T00:00:00.000Z"
-      }
-    ]);
-  });
+  expect(result).toMatchObject([
+    {
+      title: "Sample Program 1",
+      description: "Description 1",
+      category: "Genre 1",
+      image: "https://example.com/image1.jpg",
+      start: "2024-12-26T22:00:00.000Z",
+      stop: "2024-12-26T23:00:00.000Z"
+    },
+    {
+      title: "Sample Program 2",
+      description: "Description 2",
+      category: "Genre 2",
+      image: "https://example.com/image2.jpg",
+      start: "2024-12-26T23:00:00.000Z",
+      stop: "2024-12-27T00:00:00.000Z"
+    }
+  ]);
+});
 
-  it('can fetch channels', async () => {
-    axios.get.mockResolvedValue({ data: mockChannelData });
-    const result = await channels();
-    expect(result).toMatchObject([
-      {
-        lang: 'el',
-        site_id: "promohd",
-        name: "Promo HD",
-        //logo: "https://tr.static.cdn.cosmotetvott.gr/ote-prod/channel_logos/promohd-normal.png"
-      },
-      {
-        lang: 'el',
-        site_id: "vouli",
-        name: "ΒΟΥΛΗ HD",
-        //logo: "https://tr.static.cdn.cosmotetvott.gr/ote-prod/channel_logos/vouli1-normal.png"
-      }
-    ]);
-  });
-
-  it('handles error in fetching channels', async () => {
-    axios.get.mockRejectedValue(new Error('Network error'));
-    const result = await channels();
-    expect(result).toEqual([]);
-  });
+it('can handle empty guide', () => {
+  const result = parser({ date, channel, content: '{"date":"2024-12-26","categories":[],"channels":[]}' });
+  expect(result).toMatchObject([]);
 });
