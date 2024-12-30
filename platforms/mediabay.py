9fa937a83a0550a612be6a5d64f0141db9d109c0
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2024-12-30 12:44:27 -0500

    Update mediabay.py

--- platforms/mediabay.py
+++ platforms/mediabay.py
@@ -12,41 +12,52 @@ os.makedirs(output_dir, exist_ok=True)
 
 def extract_m3u8_url(channel_name):
     api_url = f"https://api.mediabay.tv/v2/channels/thread/{channel_name}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+		'Cookie': '_ga=GA1.1.1151399358.1734108649; _fbp=fb.1.1734108648923.42978377352479572; _ym_uid=1692727402762493332; _ym_d=1734108650; _ga_PDV69TQ9S4=GS1.1.1735576265.5.0.1735576265.60.0.0; SERVERID=app5; PHPSESSID=udb3fqmmv3gmu624rkj5aqnons; _ym_isad=1',
+        'Connection': 'keep-alive'
+    }
     try:
+        # Create a request with headers
+        req = urllib.request.Request(api_url, headers=headers)
+
         # Fetch data from the API
-        with urllib.request.urlopen(api_url) as url:
+        with urllib.request.urlopen(req) as url:
             data = json.loads(url.read().decode())
 
         # Extract the m3u8 URL from the JSON response
-        m3u8_url = data.get('data', [])[0].get('threadAddress', '')
-        if not m3u8_url:
-            print(f"m3u8 URL not found for channel: {channel_name}")
-            return None
+        channels_data = data.get('data', [])
+        m3u8_url = None
+        bandwidth = None
+
+        # Include the condition for 'threadType' = "7"
+        for channel in channels_data:
+            if channel.get('threadType') == "7":
+                m3u8_url = channel.get('threadAddress', '')
+                bandwidth = channel.get('bandwidth', '')
+                break
 
         # Modify URL based on bandwidth
-        if 'playlist.m3u8' in m3u8_url:
-            # Example data includes bandwidth information
-            bandwidth = determine_bandwidth(data.get('data', [])[0])
-
-            if bandwidth == 1710000:
-                m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v4a1/mono.m3u8')
-            elif bandwidth == 3020000:
-                m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v3a1/mono.m3u8')
-            elif bandwidth == 5380000:
-                m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v2a1/mono.m3u8')
-            elif bandwidth == 8010000:
-                m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v1a1/mono.m3u8')
-
-        return m3u8_url
+        if m3u8_url:
+            if 'playlist.m3u8' in m3u8_url:
+                if bandwidth == 1710000:
+                    m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v4a1/mono.m3u8')
+                elif bandwidth == 3020000:
+                    m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v3a1/mono.m3u8')
+                elif bandwidth == 5380000:
+                    m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v2a1/mono.m3u8')
+                elif bandwidth == 8010000:
+                    m3u8_url = m3u8_url.replace('playlist.m3u8', 'tracks-v1a1/mono.m3u8')
+            return m3u8_url
+        else:
+            print(f"m3u8 URL not found for channel: {channel_name}")
+            return None
 
     except Exception as e:
         print(f"An error occurred while fetching channel {channel_name}: {e}")
         return None
-
-def determine_bandwidth(data):
-    # This function should be adapted based on how bandwidth is determined in the response data
-    # Assuming the data structure contains bandwidth information like this
-    return data.get('bandwidth', None)
 
 def create_m3u8_file(m3u8_url, channel_name):
     if not m3u8_url:
