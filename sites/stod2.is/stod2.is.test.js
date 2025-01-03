087857214dddf5a6d25c9a2353715a3532ab3399
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-03 10:43:26 -0500

    Update stod2.is.test.js

--- sites/stod2.is/stod2.is.test.js
+++ sites/stod2.is/stod2.is.test.js
@@ -1,41 +1,58 @@
 const { url, parser } = require('./stod2.is.config.js')
-const fs = require('fs')
-const path = require('path')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-
 dayjs.extend(utc)
-dayjs.extend(timezone)
 
-const date = dayjs.utc('2024-12-19', 'YYYY-MM-DD').startOf('day')
-const channel = { site_id: 'stod2', xmltv_id: 'Stod2.is', lang: 'is' }
+const date = dayjs.utc('2025-01-03', 'YYYY-MM-DD').startOf('day')
+const channel = {
+  site_id: 'stod2',
+  xmltv_id: 'Stod2.is'
+}
 
 it('can generate valid url', () => {
-  expect(url({ channel, date })).toBe('https://api.stod2.is/dagskra/api/stod2/2024-12-19')
+  const generatedUrl = url({ date, channel })
+  console.log('Generated URL:', generatedUrl)
+  expect(generatedUrl).toBe('https://api.stod2.is/dagskra/api/stod2/2025-01-03')
 })
 
 it('can parse response', () => {
-  const content = fs.readFileSync(path.resolve(__dirname, '__data__/content.json'), 'utf8')
-  let results = parser({ content })
-  results = results.map(p => {
-    p.start = p.start.toJSON()
-    p.stop = p.stop.toJSON()
+  const content = `
+  {
+    "Content": [
+        {
+            "isltitill": "Heimsókn",
+            "undirtitill": "Telma Borgþórsdóttir",
+            "lysing": "Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.",
+            "adalhlutverk": "",
+            "leikstjori": "",
+            "upphaf": "2025-01-03T08:00:00Z",
+            "slott": 15
+        }
+    ]
+  }`
+
+  const result = parser({ content }).map(p => {
+    p.start = p.start.toISOString()
+    p.stop = p.stop.toISOString()
     return p
   })
 
-  expect(results).toMatchObject({
-    start: '2024-12-19T08:00:00.000Z',
-    stop: '2024-12-19T08:15:00.000Z',
-    title: 'Heimsókn',
-    sub_title: 'Signý Jóna Tryggvadóttir',
-    description: 'Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.'
-  })
+  expect(result).toMatchObject([
+    {
+      title: "Heimsókn",
+      sub_title: "Telma Borgþórsdóttir",
+      description: "Frábærir þættir með Sindra Sindrasyni sem lítur inn hjá íslenskum fagurkerum. Heimilin eru jafn ólík og þau eru mörg en eiga það þó eitt sameiginlegt að vera sett saman af alúð og smekklegheitum. Sindri hefur líka einstakt lag á að ná fram því besta í viðmælendum sínum.",
+      actors: "",
+      directors: "",
+      start: "2025-01-03T08:00:00.000Z",
+      stop: "2025-01-03T08:15:00.000Z"
+    }
+  ])
 })
 
-
 it('can handle empty guide', () => {
-  const results = parser({ content: '' })
-
-  expect(results).toMatchObject([])
+  const result = parser({
+    content: '{"Content":[]}'
+  })
+  expect(result).toMatchObject([])
 })
