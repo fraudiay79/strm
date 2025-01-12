a4ad33f2a7d15cb9bfa2e72611539b6a73b88766
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-12 11:51:02 -0500

    Update m.tv.sms.cz.config.js

--- sites/m.tv.sms.cz/m.tv.sms.cz.config.js
+++ sites/m.tv.sms.cz/m.tv.sms.cz.config.js
@@ -1,3 +1,6 @@
+// Disable TLS validation (use cautiously)
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+
 const cheerio = require('cheerio')
 const iconv = require('iconv-lite')
 const { DateTime } = require('luxon')
