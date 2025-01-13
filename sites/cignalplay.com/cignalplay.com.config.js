caedf92f4bad4610909d3711860813edca9b9e1a
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-13 09:07:30 -0500

    Update cignalplay.com.config.js

--- sites/cignalplay.com/cignalplay.com.config.js
+++ sites/cignalplay.com/cignalplay.com.config.js
@@ -25,34 +25,36 @@ module.exports = {
     return `https://live-data-store-cdn.api.pldt.firstlight.ai/content/epg?start=${start}&end=${end}&reg=ph&dt=all&client=pldt-cignal-web&pageNumber=1&pageSize=100`
 },
   async parser({ content, channel }) {
-    const shows = [];
-    let data;
+  const shows = [];
+  let data;
 
-    try {
-      if (content.trim().length === 0) {
-        throw new Error('Empty response content');
-      }
-      data = JSON.parse(content);
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      return shows; // Return empty shows array if parsing fails
+  try {
+    if (content.trim().length === 0) {
+      throw new Error('Empty response content');
     }
+    data = JSON.parse(content);
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return shows; // Return empty shows array if parsing fails
+  }
 
-    data.forEach(item => {
-      if (item.cid === channel.site_id) {
+  data.forEach(item => {
+    if (item.cid === channel.site_id) {
+      item.airing.forEach(airing => {
         const show = {
-          title: item.lon.n || '',
-          startTime: dayjs(item.sc_st_dt).utc().format(),
-          endTime: dayjs(item.sc_ed_dt).utc().format(),
-          description: item.lod.n || 'No description available',
-          category: item.genre || ''
+          title: airing.pgm.lon[0].n || '',
+          startTime: dayjs(airing.sc_st_dt).utc().format(),
+          endTime: dayjs(airing.sc_ed_dt).utc().format(),
+          description: airing.pgm.lod[0].n || 'No description available',
+          category: airing.genre || ''
         };
         shows.push(show);
-      }
-    });
+      });
+    }
+  });
 
-    return shows;
-  },
+  return shows;
+},
   async channels() {
     const url = 'https://live-data-store-cdn.api.pldt.firstlight.ai/content/epg?start=2023-01-01T00:00:00Z&end=2023-01-02T00:00:00Z&reg=ph&dt=all&client=pldt-cignal-web&pageNumber=1&pageSize=100';
     const response = await axios.get(url, {
