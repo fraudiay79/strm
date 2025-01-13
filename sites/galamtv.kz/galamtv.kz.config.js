240d7e60ce01f6d638edda843cac75dc245ce650
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-13 15:05:13 -0500

    Update galamtv.kz.config.js

--- sites/galamtv.kz/galamtv.kz.config.js
+++ sites/galamtv.kz/galamtv.kz.config.js
@@ -1,51 +1,59 @@
+const axios = require('axios')
 const dayjs = require('dayjs')
+const doFetch = require('@ntlab/sfetch')
+
 
 module.exports = {
-  site: 'galamtv.kz',
-  days: 2,
-  request: {
-    cache: {
-      ttl: 60 * 60 * 1000 // 1 hour
+    site: 'galamtv.kz',
+    channels: 'galamtv.kz.channels.xml',
+    days: 2,
+    request: {
+        cache: {
+            ttl: 60 * 60 * 1000 // 1 hour
+        }
+    },
+    url({
+        channel,
+        date
+    }) {
+        const todayEpoch = date.startOf('day').utc().valueOf()
+        const nextDayEpoch = date.add(1, 'day').startOf('day').utc().valueOf()
+        return `https://galam.server-api.lfstrm.tv/channels/${channel.site_id}/programs?period=${todayEpoch}:${nextDayEpoch}&app.version=3.5.12`
+    },
+    parser: function({
+        content
+    }) {
+        let programs = []
+        const data = JSON.parse(content)
+        const programsData = data.programs || []
+
+        programsData.forEach(program => {
+            const start = dayjs.unix(program.scheduleInfo.start)
+            const stop = dayjs.unix(program.scheduleInfo.end)
+
+            programs.push({
+                title: program.metaInfo.title,
+                description: program.metaInfo.description || '',
+                image: program.mediaInfo.thumbnails.url,
+                start,
+                stop
+            })
+        })
+
+        return programs;
+    },
+    async channels() {
+        const data = await axios
+            .get(`https://galam.server-api.lfstrm.tv/channels-now?app.version=3.3.7`)
+            .then(r => r.data)
+            .catch(console.log)
+
+        return data.channels.map(item => {
+            return {
+                lang: 'kk',
+                site_id: item.channels.id,
+                name: item.channels.info.metaInfo.title
+            }
+        })
     }
-  },
-  url({ channel }) {
-    return `https://galam.server-api.lfstrm.tv/channels/${channel.site_id}/programs?app.version=3.3.7`
-  },
-  parser: function ({ content }) {
-    let programs = []
-    const items = parseItems(content)
-    items.forEach(item => {
-      programs.push({
-        title: item.title,
-        description: item.description,
-        start: parseStart(item),
-        stop: parseStop(item)
-      })
-    })
-
-    return programs
-  },
-  async channels() {
-    const axios = require('axios')
-    const data = await axios
-      .get(`https://galam.server-api.lfstrm.tv/channels-now?app.version=3.3.7`)
-      .then(r => r.data)
-      .catch(console.log)
-
-    return data.channels.map(item => {
-      return {
-        lang: 'kk',
-        site_id: item.channels.id,
-        name: item.channels.info.metaInfo.title
-      }
-    })
-  }
-}
-
-function parseItems(content, channel) {
-  const data = JSON.parse(content)
-  if (!data || !Array.isArray(data.channels)) return []
-  const channelData = data.channels.find(i => i.id === channel.site_id)
-
-  return channelData && Array.isArray(channelData.programs) ? channelData.programs : []
 }
