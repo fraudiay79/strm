417dccd857c08cd44e3dbb17323119183b59fbf8
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-15 08:32:42 -0500

    Update snrt.ma.config.js

--- sites/snrt.ma/snrt.ma.config.js
+++ sites/snrt.ma/snrt.ma.config.js
@@ -1,117 +1,119 @@
-const cheerio = require('cheerio')
-const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-const customParseFormat = require('dayjs/plugin/customParseFormat')
+const cheerio = require('cheerio');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+const axios = require('axios');
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(customParseFormat)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 
-const tz = 'Africa/Casablanca'
+const tz = 'Africa/Casablanca';
 
 module.exports = {
   site: 'snrt.ma',
   days: 2,
   url({ channel }) {
-    return `https://www.snrt.ma/ar/node/${channel.site_id}`
+    return `https://www.snrt.ma/ar/node/${channel.site_id}`;
   },
   request: {
     cache: {
       ttl: 24 * 60 * 60 * 1000 // 1 day
     }
   },
-  parser({ content, date }) {
-    const [$, items] = parseItems(content)
-    const programs = items.map(item => {
-      const $item = $(item)
-      const start = parseStart($item)
-      const description = await parseDescription($item)
+  async parser({ content, date }) {
+    const [$, items] = parseItems(content);
+    const programs = await Promise.all(items.map(async item => {
+      const $item = $(item);
+      const start = parseStart($item);
+      const description = await parseDescription($item);
       return {
         title: parseTitle($item),
         description,
         image: parseImage($item),
         category: parseCategory($item),
         start
-      }
-    }).filter(item => item.start).sort((a, b) => a.start - b.start)
+      };
+    }));
+    
+    programs.filter(item => item.start).sort((a, b) => a.start - b.start);
+    
     // fill start-stop
     for (let i = 0; i < programs.length; i++) {
       if (i < programs.length - 1) {
-        programs[i].stop = programs[i + 1].start
+        programs[i].stop = programs[i + 1].start;
       } else {
         programs[i].stop = dayjs.tz(
           `${date.add(1, 'd').format('YYYY-MM-DD')} 00:00`,
           'YYYY-MM-DD HH:mm',
           tz
-        )
+        );
       }
     }
 
-    return programs.filter(p => p.start.isSame(date, 'd'))
+    return programs.filter(p => p.start.isSame(date, 'd'));
   },
   async channels({ lang = 'ar' }) {
-    const axios = require('axios')
     const result = await axios
       .get('https://www.snrt.ma/ar/node/1208')
       .then(response => response.data)
-      .catch(console.error)
+      .catch(console.error);
 
-    const $ = cheerio.load(result)
-    const items = $('.channels-row h4').toArray()
+    const $ = cheerio.load(result);
+    const items = $('.channels-row h4').toArray();
     const channels = items.map(item => {
-      const $item = $(item)
-      const url = $item.find('a').attr('href')
+      const $item = $(item);
+      const url = $item.find('a').attr('href');
       return {
         lang,
         site_id: url.substr(url.lastIndexOf('/') + 1),
         name: $item.find('img').attr('alt')
-      }
-    })
+      };
+    });
 
-    return channels
+    return channels;
   }
-}
+};
 
 function parseStart($item) {
-  const date = $item.attr('class').match(/\d{8}/)[0]
-  const time = $item.find('.grille-time').text().trim()
+  const date = $item.attr('class').match(/\d{8}/)[0];
+  const time = $item.find('.grille-time').text().trim();
   if (time) {
-    return dayjs.tz(`${date} ${time.replace('H', ':')}`, 'YYYYMMDD HH:mm', tz)
+    return dayjs.tz(`${date} ${time.replace('H', ':')}`, 'YYYYMMDD HH:mm', tz);
   }
 }
 
 function parseTitle($item) {
-  return $item.find('.program-title-sm').text().trim()
+  return $item.find('.program-title-sm').text().trim();
 }
 
 async function parseDescription($item) {
-    const link = $item.find('.grille-content a').attr('href')
-    if (link) {
-        try {
-            const response = await axios.get(link)
-            const $ = cheerio.load(response.data)
-            const description = $('.program-description-sm').text().trim()
-            return description
-        } catch (error) {
-            console.error('Error fetching the description:', error)
-            return ''
-        }
+  const link = $item.find('.grille-content a').attr('href');
+  if (link) {
+    try {
+      const response = await axios.get(link);
+      const $ = cheerio.load(response.data);
+      const description = $('.program-description-sm').text().trim();
+      return description;
+    } catch (error) {
+      console.error('Error fetching the description:', error);
+      return '';
     }
-    return ''
+  }
+  return '';
 }
 
 function parseImage($item) {
-    const imgSrc = $item.find('img.img-fluid').attr('src');
-    return imgSrc ? imgSrc.trim() : '';
+  const imgSrc = $item.find('img.img-fluid').attr('src');
+  return imgSrc ? imgSrc.trim() : '';
 }
 
 function parseCategory($item) {
-  return $item.find('.genre-first').text().trim()
+  return $item.find('.genre-first').text().trim();
 }
 
 function parseItems(content) {
-  const $ = cheerio.load(content)
-
-  return [$, $('.grille-line').toArray()]
+  const $ = cheerio.load(content);
+  return [$, $('.grille-line').toArray()];
 }
