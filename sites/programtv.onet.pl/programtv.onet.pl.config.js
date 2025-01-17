57ded3e304230fbb6baa766302ecbb49ca688bc9
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-17 17:28:28 -0500

    Update programtv.onet.pl.config.js

--- sites/programtv.onet.pl/programtv.onet.pl.config.js
+++ sites/programtv.onet.pl/programtv.onet.pl.config.js
@@ -1,33 +1,33 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { DateTime } = require('luxon');
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { DateTime } = require('luxon')
 
 module.exports = {
   delay: 5000,
   site: 'programtv.onet.pl',
   days: 2,
   url: function ({ date, channel }) {
-    const currDate = DateTime.now().toUTC().startOf('day');
+    const currDate = DateTime.now().toUTC().startOf('day')
     const day = date.diff(currDate, 'd')
 
-    return `https://programtv.onet.pl/program-tv/${channel.site_id}?dzien=${day}`;
+    return `https://programtv.onet.pl/program-tv/${channel.site_id}?dzien=${day}`
   },
   parser: async function ({ content, date }) {
-    const programs = [];
-    const items = parseItems(content);
+    const programs = []
+    const items = parseItems(content)
     for (const item of items) {
-      const prev = programs[programs.length - 1];
-      const $item = cheerio.load(item);
-      let start = parseStart($item, date);
+      const prev = programs[programs.length - 1]
+      const $item = cheerio.load(item)
+      let start = parseStart($item, date)
       if (prev) {
         if (start < prev.start) {
-          start = start.plus({ days: 1 });
-          date = date.add(1, 'd');
+          start = start.plus({ days: 1 })
+          date = date.add(1, 'd')
         }
-        prev.stop = start;
+        prev.stop = start
       }
-      const stop = start.plus({ hours: 1 });
-      const details = await loadProgramDetails($item);
+      const stop = start.plus({ hours: 1 })
+      const details = await loadProgramDetails($item)
       programs.push({
         title: parseTitle($item),
         description: details.description,
@@ -40,49 +40,49 @@ module.exports = {
         episode: details.episode,
         start,
         stop
-      });
+      })
     }
 
-    return programs;
+    return programs
   },
   async channels() {
     const data = await axios
       .get('https://programtv.onet.pl/stacje')
       .then(r => r.data)
-      .catch(console.log);
+      .catch(console.log)
 
-    let channels = [];
+    let channels = []
 
-    const $ = cheerio.load(data);
+    const $ = cheerio.load(data)
     $('ul.channelList a').each((i, el) => {
-      const name = $(el).text();
-      const url = $(el).attr('href');
-      const [, site_id] = url.match(/^\/program-tv\/(.*)$/i);
+      const name = $(el).text()
+      const url = $(el).attr('href')
+      const [, site_id] = url.match(/^\/program-tv\/(.*)$/i)
 
       channels.push({
         lang: 'pl',
         site_id,
-        name,
-      });
-    });
+        name
+      })
+    })
 
-    return channels;
+    return channels
   }
-};
+}
 
 async function loadProgramDetails($item) {
-  const programId = $item('a').attr('href');
+  const programId = $item('a').attr('href')
   const data = await axios
     .get(`https://programtv.onet.pl/${programId}`)
     .then(r => r.data)
-    .catch(console.error);
-  if (!data) return Promise.resolve({});
+    .catch(console.error)
+  if (!data) return Promise.resolve({})
   
-  const $ = cheerio.load(data);
+  const $ = cheerio.load(data)
 
   // Find the JSON-LD script containing the data
-  const script = $('script[type="application/ld+json"]').html();
-  const jsonData = JSON.parse(script);
+  const script = $('script[type="application/ld+json"]').html()
+  const jsonData = JSON.parse(script)
 
   return Promise.resolve({
     icon: jsonData.image.url,
@@ -93,22 +93,22 @@ async function loadProgramDetails($item) {
     sub_title: jsonData.name,
     season: jsonData.workPerformed.partOfSeason ? jsonData.workPerformed.partOfSeason.seasonNumber : null,
     episode: jsonData.workPerformed.episodeNumber
-  });
+  })
 }
 
 function parseStart($item, date) {
-  const timeString = $item('.hours > .hour').text();
-  const dateString = `${date.toFormat('MM/dd/yyyy')} ${timeString}`;
+  const timeString = $item('.hours > .hour').text()
+  const dateString = `${date.format('MM/DD/YYYY')} ${timeString}`
 
-  return DateTime.fromFormat(dateString, 'MM/dd/yyyy HH:mm', { zone: 'Europe/Warsaw' }).toUTC();
+  return DateTime.fromFormat(dateString, 'MM/dd/yyyy HH:mm', { zone: 'Europe/Warsaw' }).toUTC()
 }
 
 function parseTitle($item) {
-  return $item('.titles > a').text().trim();
+  return $item('.titles > a').text().trim()
 }
 
 function parseItems(content) {
-  const $ = cheerio.load(content);
+  const $ = cheerio.load(content)
 
-  return $('#channelTV > section > div.emissions > ul > li').toArray();
+  return $('#channelTV > section > div.emissions > ul > li').toArray()
 }
