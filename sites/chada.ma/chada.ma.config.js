8d0abb008ddccc157a76ffe5fa850f5f6fb19361
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-17 03:14:15 -0500

    Update chada.ma.config.js

--- sites/chada.ma/chada.ma.config.js
+++ sites/chada.ma/chada.ma.config.js
@@ -31,11 +31,11 @@ module.exports = {
       const titleElement = $(element).next('div').next('h3')
       const title = titleElement.text().trim()
       const description = titleElement.next('div').text().trim() || 'No description available'
-      const image = $(element).next('div').find('img.programthumb').attr('src')
+      const icon = $(element).next('div').find('img.programthumb').attr('src')
       programs.push({
         title,
         description,
-        image,
+        icon,
         start,
         stop
       })
