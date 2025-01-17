e14ad2496ec3026bd9ba61599e68812d045335a4
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-17 03:39:01 -0500

    Update elcinema.com.config.js

--- sites/elcinema.com/elcinema.com.config.js
+++ sites/elcinema.com/elcinema.com.config.js
@@ -33,7 +33,7 @@ module.exports = {
         title: parseTitle(item),
         description: parseDescription(item),
         category: parseCategory(item),
-        image: parseImage(item),
+        icon: parseImage(item),
         start,
         stop
       })
