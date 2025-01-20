aa76e7a7b96b331cdfe15512c7b9a78881e6a0b8
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-20 10:31:17 -0500

    Update telemach.me.config.js

--- sites/telemach.me/telemach.me.config.js
+++ sites/telemach.me/telemach.me.config.js
@@ -1,76 +1,119 @@
 const dayjs = require('dayjs')
 const axios = require('axios')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-const customParseFormat = require('dayjs/plugin/customParseFormat')
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(customParseFormat)
+const BASIC_TOKEN =
+  'MjdlMTFmNWUtODhlMi00OGU0LWJkNDItOGUxNWFiYmM2NmY1OjEyejJzMXJ3bXdhZmsxMGNkdzl0cjloOWFjYjZwdjJoZDhscXZ0aGc='
+
+let session
 
 module.exports = {
   site: 'telemach.me',
-  days: 10,
+  days: 3,
   url({ channel, date }) {
-    return `https://api-web.ug-be.cdn.united.cloud/v1/public/events/epg?fromTime=${date.format('YYYY-MM-DD')}T00:00:00-05:00&toTime=${date.add(1, 'days').format('YYYY-MM-DD')}T23:59:59-05:00&communityId=5&languageId=10001&cid=${channel.site_id}`
+    return `https://api-web.ug-be.cdn.united.cloud/v1/public/events/epg?fromTime=${date.format(
+      'YYYY-MM-DDTHH:mm:ss-00:00'
+    )}&toTime=${date
+      .add(1, 'days')
+      .subtract(1, 's')
+      .format('YYYY-MM-DDTHH:mm:ss-00:00')}&communityId=12&languageId=59&cid=${channel.site_id}`
   },
   request: {
-    headers: {
-      Referer: 'https://epg.telemach.me/',
-      Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidWMtaW5mby1zZXJ2aWNlIl0sInNjb3BlIjpbInJlYWQiXSwiZXhwIjoxNzM3MzU2MTUxLCJhdXRob3JpdGllcyI6WyJST0xFX1BVQkxJQ19FUEciXSwianRpIjoiM1VjbXUzWmN2Tnc0bHJlVFlqYVRPNEppVWZJIiwiY2xpZW50X2lkIjoiMjdlMTFmNWUtODhlMi00OGU0LWJkNDItOGUxNWFiYmM2NmY1In0.RPTFKDirB2OxWREV1ECLCqKmCiAOntwnvaJ60NsK-CyWxla54Vd2Fa2XF_XdL3jQmeq3cYIbieUTXtW_83z6YyQFkUcZwGLwmHlC8Ui21MX_yFL9FVL-gQuQMzUi6uS11wZD-oFIzMxuF7wHzLKnTFj2qfvH_ffqsV0nf4oxuWyJSL2W9JobOtyXPeUQz08uKCR13V2XV2xTy4ZJNxz7GKSX_zqF1vxifAra6iovnXZ-DfqAiimfB4pqIR2tEtY8HFm0EfrFMheaSFpycimv5SYwy3FArHFxtRXf2UiDgHezkrCytb6Rnmrs3No8HCkJ4P0sCE1kOV59yNzKmLiMzQ'
-    }
-  },
-  parser: function ({ content }) {
-  const programs = []
-  try {
-    const data = JSON.parse(content);
-    for (const channelId in data) {
-      if (Array.isArray(data[channelId])) {
-        data[channelId].forEach(item => {
-          programs.push({
-            title: item.title,
-            description: item.shortDescription,
-            categories: item.categories || null,
-            icon: parseImages(item),
-            season: item.seasonNumber || null,
-            episode: item.episodeNumber || null,
-            start: item.startTime,
-            stop: item.endTime
-          })
-        })
+    async headers() {
+      if (!session) {
+        session = await loadSessionDetails()
+        if (!session || !session.access_token) return null
+      }
+
+      return {
+        Authorization: `Bearer ${session.access_token}`
       }
     }
-  } catch (error) {
-    console.error('Error parsing content:', error)
-  }
-
-  return programs
-},
-  async channels() {
+  },
+  parser({ content }) {
     try {
-      const response = await axios.get('https://api-web.ug-be.cdn.united.cloud/v1/public/channels?channelType=TV&communityId=5&languageId=10001&imageSize=L', {
-        headers: {
-          Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidWMtaW5mby1zZXJ2aWNlIl0sInNjb3BlIjpbInJlYWQiXSwiZXhwIjoxNzM3MzU2MTUxLCJhdXRob3JpdGllcyI6WyJST0xFX1BVQkxJQ19FUEciXSwianRpIjoiM1VjbXUzWmN2Tnc0bHJlVFlqYVRPNEppVWZJIiwiY2xpZW50X2lkIjoiMjdlMTFmNWUtODhlMi00OGU0LWJkNDItOGUxNWFiYmM2NmY1In0.RPTFKDirB2OxWREV1ECLCqKmCiAOntwnvaJ60NsK-CyWxla54Vd2Fa2XF_XdL3jQmeq3cYIbieUTXtW_83z6YyQFkUcZwGLwmHlC8Ui21MX_yFL9FVL-gQuQMzUi6uS11wZD-oFIzMxuF7wHzLKnTFj2qfvH_ffqsV0nf4oxuWyJSL2W9JobOtyXPeUQz08uKCR13V2XV2xTy4ZJNxz7GKSX_zqF1vxifAra6iovnXZ-DfqAiimfB4pqIR2tEtY8HFm0EfrFMheaSFpycimv5SYwy3FArHFxtRXf2UiDgHezkrCytb6Rnmrs3No8HCkJ4P0sCE1kOV59yNzKmLiMzQ'
+      const programs = []
+      const data = JSON.parse(content)
+      for (const channelId in data) {
+        if (Array.isArray(data[channelId])) {
+          data[channelId].forEach(item => {
+            programs.push({
+              title: item.title,
+              description: item.shortDescription,
+              icon: parseImage(item),
+              season: item.seasonNumber,
+              episode: item.episodeNumber,
+              start: dayjs(item.startTime),
+              stop: dayjs(item.endTime)
+            })
+          })
         }
-      })
+      }
 
-      const data = response.data
+      return programs
+    } catch {
+      return []
+    }
+  },
+  async channels() {
+    const countries = {
+      ba: { communityId: '12', languageId: '59', lang: 'bs' },
+      gr: { communityId: '35', languageId: '164', lang: 'el' },
+      me: { communityId: '5', languageId: '10001', lang: 'cnr' },
+      rs: { communityId: '1', languageId: '404', lang: 'sr' },
+      si: { communityId: '8', languageId: '386', lang: 'sl' }
+    }
+	  
+    const session = await loadSessionDetails()
+    if (!session || !session.access_token) return null
+	
+	let channels = []
+    for (let country in countries) {
+      const config = countries[country]
+      const lang = config.lang
 
-      return data.map(item => ({
-        lang: 'cnr',
+    try {
+      const data = await axios.get(
+        `https://api-web.ug-be.cdn.united.cloud/v1/public/channels?channelType=TV&communityId=${config.communityId}&languageId=${config.languageId}&imageSize=L`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.access_token}`
+          }
+        }
+      )
+      
+      const channelData = data.data.map(item => ({
+        lang,
         site_id: item.id,
         name: item.name
       }))
+      
+      channels = [...channels, ...channelData]
     } catch (error) {
-      console.error('Error fetching channels:', error)
-      return []
+      console.error(error)
     }
   }
+
+  return channels
+}
 }
 
-function parseImages(item) {
-  const baseURL = 'https://images-web.ug-be.cdn.united.cloud/'
-  return Array.isArray(item?.images) && item.images.length > 0
-    ? `${baseURL}${item.images[0].path}&width=640&height=360`
-    : ''
+function parseImage(item) {
+  const baseURL = 'https://images-web.ug-be.cdn.united.cloud'
+
+  return Array.isArray(item?.images) && item.images[0] ? `${baseURL}${item.images[0].path}` : null
+}
+
+function loadSessionDetails() {
+  return axios
+    .post(
+      'https://api-web.ug-be.cdn.united.cloud/oauth/token?grant_type=client_credentials',
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${BASIC_TOKEN}`
+        }
+      }
+    )
+    .then(r => r.data)
+    .catch(console.log)
 }
