80b7ebd8e6cae5da2a36fb06d8997d1fd915e936
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-21 16:08:40 -0500

    Update antiktv.sk.config.js

--- sites/antiktv.sk/antiktv.sk.config.js
+++ sites/antiktv.sk/antiktv.sk.config.js
@@ -21,25 +21,29 @@ module.exports = {
     return `https://antiktv.sk/en/epg/epg/?action=getEpgList&options[day]=${formattedDate}&options[filters][channels][]=${channel.site_id}&isAjax=true`
   },
   parser: function ({ content }) {
-  let programs = [];
-  const data = JSON.parse(content);
-  Object.keys(data.data).forEach(date => {
-    data.data[date].forEach(channelData => {
-      channelData.epg.forEach(item => {
-        const start = dayjs(item.Start).utc().toISOString();
-        const stop = dayjs(item.Stop).utc().toISOString();
-        programs.push({
-          title: item.Title,
-          description: item.Description || 'No description available',
-          category: item.Genres.join(', '),
-          icon: item.Icon,
-          start,
-          stop
-        });
-      });
-    });
-  });
-  return programs;
+  let programs = []
+  const data = JSON.parse(content)
+
+  for (const date in data.data) {
+    if (Array.isArray(data.data[date])) {
+      data.data[date].forEach(channelData => {
+        channelData.epg.forEach(item => {
+          const start = dayjs(item.Start).utc().toISOString()
+          const stop = dayjs(item.Stop).utc().toISOString()
+          programs.push({
+            title: item.Title,
+            description: item.Description || 'No description available',
+            category: item.Genres.join(', '),
+            icon: item.Icon,
+            start,
+            stop
+          })
+        })
+      })
+    }
+  }
+
+  return programs
 },
   async channels() {
     let channels = []
