d0c4d0454b5ea5c0618ea04377222e43315f3ab6
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-21 12:29:36 -0500

    Create antiktv.sk.config.js

---
+++ sites/antiktv.sk/antiktv.sk.config.js
@@ -0,0 +1,64 @@
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+const axios = require('axios')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
+
+module.exports = {
+  site: 'antiktv.sk',
+  days: 2,
+  request: {
+    cache: {
+      ttl: 60 * 60 * 1000 // 1 hour
+    }
+  },
+  url({ channel, date }) {
+    const formattedDate = dayjs(date).format('YYYY-MM-DD')
+    return `https://antiktv.sk/en/epg/epg/?action=getEpgList&options[day]=${formattedDate}&options[filters][channels][]=${channel.site_id}&isAjax=true`
+  },
+  parser: function ({ content }) {
+    const programs = []
+
+    const data = JSON.parse(content).data
+    Object.keys(data).forEach(date => {
+      data[date].forEach(channel => {
+        channel.epg.forEach(item => {
+          const programData = {
+            title: item.Title,
+            subtitle: item.Subtitle || null,
+            description: item.Description || null,
+            icon: item.Icon || null,
+            category: item.Genres || [],
+            start: dayjs.tz(item.Start, 'Europe/Prague'),
+            stop: dayjs.tz(item.Stop, 'Europe/Prague')
+          }
+
+          programs.push(programData)
+        })
+      })
+    })
+
+    return programs
+  },
+  async channels(date) {
+    try {
+      const formattedDate = dayjs(date).format('YYYY-MM-DD')
+      const response = await axios.get(`https://antiktv.sk/en/epg/epg/?action=getEpgList&options[day]=${formattedDate}&isAjax=true`)
+      const data = response.data
+      return Object.values(data.filters.initArray.channels).map(channel => {
+        return {
+          lang: 'sk',
+          name: channel.name,
+          site_id: channel.id_content
+        }
+      })
+    } catch (error) {
+      console.error('Error fetching channels:', error)
+      return []
+    }
+  }
+}
