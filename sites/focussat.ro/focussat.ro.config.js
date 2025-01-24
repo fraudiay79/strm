7f5021df7015745f6fb1c1ede2c47c318f7fbb2b
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-24 01:06:12 -0500

    Update focussat.ro.config.js

--- sites/focussat.ro/focussat.ro.config.js
+++ sites/focussat.ro/focussat.ro.config.js
@@ -35,9 +35,9 @@ module.exports = {
       }
     }
   },
-  parser: async function ({ content, date }) {
+  parser: async function ({ content }) {
     let programs = []
-    const items = parseItems(content, date)
+    const items = JSON.parse(content)
 
     for (let item of items) {
       const detail = await loadProgramDetails(item)
@@ -109,26 +109,6 @@ function parseImages(item) {
 function parseRoles(detail, role_name) {
   if (!detail.params || !detail.params.credits) return null
   return detail.params.credits.filter(role => role.role === role_name).map(role => role.person)
-}
-
-function parseItems(content, date) {
-  try {
-    if (!content || !content.epg) return []
-
-    const items = []
-    Object.keys(content.epg).forEach(channelId => {
-      content.epg[channelId].forEach(program => {
-        if (program?.params?.start && date.isSame(dayjs.utc(program.params.start, 'YYYY-MM-DDTHH:mm:ss[Z]'), 'd')) {
-          items.push(program)
-        }
-      })
-    })
-
-    return items
-  } catch (err) {
-    console.log(err)
-    return []
-  }
 }
 
 function loadSessionDetails() {
