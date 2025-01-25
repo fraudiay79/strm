524b0c33e23c12420f6c27ba09626fc2a6a9670a
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-25 13:31:04 -0500

    Update magentatv_test.sk.config.js

--- sites/magentatv_test.sk/magentatv_test.sk.config.js
+++ sites/magentatv_test.sk/magentatv_test.sk.config.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-const dayjs = require('dayjs');
+const axios = require('axios')
+const dayjs = require('dayjs')
 
-let session;
+let session
 
-const API_ENDPOINT = 'https://skgo.magio.tv/v2/television';
+const API_ENDPOINT = 'https://skgo.magio.tv/v2/television'
 
 async function fetchTokens(url) {
   try {
@@ -16,20 +16,20 @@ async function fetchTokens(url) {
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'cross-site'
       }
-    });
+    })
     if (response.status === 200) {
-      console.log('Tokens retrieved successfully:', response.data); // Debugging line
-      const data = response.data;
-      const accessToken = data.token.accessToken;
-      const refreshToken = data.token.refreshToken;
-      return { accessToken, refreshToken };
+      //console.log('Tokens retrieved successfully:', response.data) // Debugging line
+      const data = response.data
+      const accessToken = data.token.accessToken
+      const refreshToken = data.token.refreshToken
+      return { accessToken, refreshToken }
     } else {
-      console.error(`Failed to retrieve tokens, status code: ${response.status}`);
-      return null;
+      console.error(`Failed to retrieve tokens, status code: ${response.status}`)
+      return null
     }
   } catch (error) {
-    console.error('Error fetching tokens:', error);
-    return null;
+    console.error('Error fetching tokens:', error)
+    return null
   }
 }
 
@@ -39,29 +39,29 @@ module.exports = {
   request: {
     async headers() {
       if (!session) {
-        session = await loadSessionDetails();
-        if (!session || !session.accessToken) return null;
+        session = await loadSessionDetails()
+        if (!session || !session.accessToken) return null
       }
 
       return {
         Authorization: `Bearer ${session.accessToken}`,
         Referer: 'https://magiogo.sk/',
         Origin: 'https://magiogo.sk'
-      };
+      }
     },
     cache: {
       ttl: 24 * 60 * 60 * 1000 // 1 day
     }
   },
   url({ channel, date }) {
-    return `${API_ENDPOINT}/epg?filter=channel.id=in=(${channel.site_id});startTime=ge=${date.format('YYYY-MM-DDTHH:mm:ss.000')};startTime=le=${date.add(1, 'days').subtract(1, 's').format('YYYY-MM-DDTHH:mm:ss.000')}&lang=SK`;
+    return `${API_ENDPOINT}/epg?filter=channel.id=in=(${channel.site_id});startTime=ge=${date.format('YYYY-MM-DDTHH:mm:ss.000')};startTime=le=${date.add(1, 'days').subtract(1, 's').format('YYYY-MM-DDTHH:mm:ss.000')}&lang=SK`
   },
   parser({ content }) {
     try {
-      const programs = [];
-      const data = JSON.parse(content);
+      const programs = []
+      const data = JSON.parse(content)
       for (const channelItem of data.items) {
-        const channelId = channelItem.channel.channelId;
+        const channelId = channelItem.channel.channelId
         if (Array.isArray(channelItem.programs)) {
           channelItem.programs.forEach(item => {
             programs.push({
@@ -75,23 +75,23 @@ module.exports = {
               directors: item.program.programRole.directors.map(director => director.fullName).join(', '),
               start: dayjs(item.startTime),
               stop: dayjs(item.endTime)
-            });
-          });
+            })
+          })
         }
       }
 
-      return programs;
+      return programs
     } catch (error) {
-      console.error('Error parsing content:', error);
-      return [];
+      console.error('Error parsing content:', error)
+      return []
     }
   },
   async channels() {
     if (!session || !session.accessToken) {
-      session = await loadSessionDetails();
+      session = await loadSessionDetails()
       if (!session || !session.accessToken) {
-        console.error('Error: Unable to retrieve session or accessToken.');
-        return [];
+        console.error('Error: Unable to retrieve session or accessToken.')
+        return []
       }
     }
 
@@ -104,26 +104,26 @@ module.exports = {
         }
       })
       .then(r => r.data)
-      .catch(console.log);
+      .catch(console.log)
 
     return data.items.map(item => {
       return {
         lang: 'sk',
         site_id: item.channel.channelId,
         name: item.channel.name
-      };
-    });
+      }
+    })
   }
-};
+}
 
 async function loadSessionDetails() {
-  const url = 'https://skgo.magio.tv/v2/auth/init?dsid=Netscape.1737828170898.0.49497112051955927&deviceName=Web%20Browser&deviceType=OTT_WIN&osVersion=0.0.0&appVersion=4.0.21-hf.0&language=SK';
-  const tokens = await fetchTokens(url);
-  console.log('Loaded session details:', tokens); // Debugging line
+  const url = 'https://skgo.magio.tv/v2/auth/init'
+  const tokens = await fetchTokens(url)
+  //console.log('Loaded session details:', tokens) // Debugging line
   if (tokens) {
-    return tokens;
+    return tokens
   } else {
-    console.error('Error loading session details.');
-    return null;
+    console.error('Error loading session details.')
+    return null
   }
 }
