8a4c9cc9ff109ecea80e50d5ff58f4af94191ea5
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-25 14:48:23 -0500

    Update focussat.ro.config.js

--- sites/focussat.ro/focussat.ro.config.js
+++ sites/focussat.ro/focussat.ro.config.js
@@ -1,42 +1,43 @@
-const axios = require('axios');
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
+const dayjs = require('dayjs')
+const axios = require('axios')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
 
-const API_ENDPOINT = 'https://tvapi.solocoo.tv/v1';
-let session;
+const API_ENDPOINT = 'https://tvapi.solocoo.tv/v1'
+
+let session
 
 module.exports = {
   site: 'focussat.ro',
   days: 2,
   delay: 5000,
   url({ channel, date }) {
-    return `${API_ENDPOINT}/schedule?channels=${channel.site_id}&from=${date.format('YYYY-MM-DDTHH:mm:ss.SSS')}Z&until=${date.add(1, 'days').format('YYYY-MM-DDTHH:mm:ss.SSS')}Z`;
+    return `${API_ENDPOINT}/schedule?channels=${channel.site_id}&from=${date.format('YYYY-MM-DDTHH:mm:ss.SSS')}Z&until=${date.add(1, 'days').format('YYYY-MM-DDTHH:mm:ss.SSS')}Z`
   },
   request: {
     async headers() {
       if (!session) {
-        session = await loadSessionDetails();
-        if (!session || !session.token) return null;
+        session = await loadSessionDetails()
+        if (!session || !session.token) return null
       }
 
       return {
         Authorization: `Bearer ${session.token}`,
         Origin: 'https://livetv.focussat.ro',
         Referer: 'https://livetv.focussat.ro/'
-      };
+      }
     }
   },
   parser: async function ({ content }) {
-    let programs = [];
-    const items = parseItems(content);
+    let programs = []
+    const items = parseItems(content)
     for (const item of items) {
-      const detail = await loadProgramDetails(item.id);
+      const detail = await loadProgramDetails(item.id)
       programs.push({
         id: item.id,
         title: detail.title || item.title,
@@ -48,124 +49,97 @@ module.exports = {
         episode: item.params ? item.params.seriesEpisode : null,
         start: parseStart(item),
         stop: parseStop(item)
-      });
+      })
     }
 
-    return programs;
+    return programs
   },
   async channels() {
-    const session = await loadSessionDetails();
-    if (!session || !session.token) throw new Error('The session token is missing');
+    const session = await loadSessionDetails()
+    if (!session || !session.token) throw new Error('The session token is missing')
 
     const data = await axios.get(`${API_ENDPOINT}/bouquet`, {
       headers: {
         Authorization: `Bearer ${session.token}`
       }
-    }).then(r => r.data).catch(console.error);
+    }).then(r => r.data).catch(console.error)
 
     return data.channels.map(item => ({
       lang: 'ro',
       site_id: item.assetInfo.id,
       name: item.assetInfo.title
-    }));
-  }
-};
-
-async function fetchSessionData(ssoToken, token) {
-  const url = 'https://tvapi.solocoo.tv/v1/session';
-
-  try {
-    const response = await axios.post(url, {
-      "userName": "Demo Chrome 131.0.0.0",
-      "demo": true,
-      "userLogin": "demo_fsro_fNxR2lPk118zQE9K3d9uWg",
-      "userId": "9c6e0419-f77f-0b59-f309-9cfc8175b954",
-      "countryCode": "RO",
-      "communityName": "Focus Sat",
-      "ssoToken": ssoToken,
-      "update": true,
-      "language": "en_US",
-      "brandName": "Focus Sat",
-      "token": token,
-      "consent": false
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const data = response.data;
-
-    const ssoTokenResponse = data.ssoToken;
-    const provisionData = data.token; // Assigning token to provisionData
-
-    console.log('Fetched session data successfully:', { ssoToken: ssoTokenResponse, provisionData });
-    return { ssoToken: ssoTokenResponse, provisionData };
-  } catch (error) {
-    console.error('Fetch error:', error);
-    return null;
+    }))
   }
 }
 
-async function loadSessionDetails() {
-  // Assume these values are dynamically obtained for each session
-  const dynamicSsoToken = "your_dynamic_ssoToken_here";
-  const dynamicToken = "your_dynamic_token_here";
+async function loadProgramDetails(id) {
+  if (!id) return {}
+  const url = `${API_ENDPOINT}/assets/${id}`
+  const data = await axios.get(url, { headers: {
+    Authorization: `Bearer ${session.token}`,
+    Origin: 'https://livetv.focussat.ro',
+    Referer: 'https://livetv.focussat.ro/'
+  } }).then(r => r.data).catch(error => {
+    console.log(error)
+    return null
+  })
 
-  const sessionData = await fetchSessionData(dynamicSsoToken, dynamicToken);
-  if (!sessionData) return null;
+  const landscapeImage = data.images.find(img => img.type === 'la')?.url
 
-  const { ssoToken, provisionData } = sessionData;
-
-  try {
-    const response = await axios.post(`${API_ENDPOINT}/session`, {
-      ssoToken: ssoToken,
-      osVersion: 'Windows 10',
-      deviceModel: 'Chrome',
-      deviceType: 'PC',
-      deviceSerial: 'w112c2a90-da0a-11ef-b249-ad4a0ed7b0a7',
-      deviceOem: 'Chrome',
-      devicePrettyName: 'Chrome 131.0.0.0',
-      appVersion: '12.0',
-      language: 'en_US',
-      brand: 'fsro',
-      memberId: '0',
-      featureLevel: 6,
-      provisionData: provisionData
-    });
-
-    const data = response.data;
-    console.log('Loaded session details successfully:', data);
-    return data;
-  } catch (error) {
-    console.error('Load session details error:', error);
-    return null;
+  return {
+    id: data.id,
+    title: data.title,
+    description: data.desc,
+    icon: landscapeImage,
+    actors: data.params.credits
+      .filter(credit => credit.roleLabel === 'sg.ui.role.Cast')
+      .map(credit => credit.person),
+    directors: data.params.credits
+      .filter(credit => credit.roleLabel === 'sg.ui.role.Director')
+      .map(credit => credit.person)
   }
 }
 
-// Helper functions
 function parseImages(item) {
   return Array.isArray(item.images)
     ? item.images.find(i => i.type === 'la')?.url || ''
-    : '';
+    : ''
 }
 
 function parseStart(item) {
-  return item?.params?.start ? dayjs.utc(item.params.start, 'YYYY-MM-DDTHH:mm:ss[Z]') : null;
+  return item?.params?.start ? dayjs.utc(item.params.start, 'YYYY-MM-DDTHH:mm:ss[Z]') : null
 }
 
 function parseStop(item) {
-  return item?.params?.end ? dayjs.utc(item.params.end, 'YYYY-MM-DDTHH:mm:ss[Z]') : null;
+  return item?.params?.end ? dayjs.utc(item.params.end, 'YYYY-MM-DDTHH:mm:ss[Z]') : null
 }
 
 function parseRoles(detail, role_name) {
-  if (!detail.credits) return [];
+  if (!detail.credits) return []
   return detail.credits
     .filter(role => role.roleLabel === role_name)
-    .map(role => role.person);
+    .map(role => role.person)
 }
 
 function parseItems(content) {
-  const parsed = JSON.parse(content);
-  return Array.isArray(parsed) ? parsed : [];
+  const parsed = JSON.parse(content)
+  return Array.isArray(parsed) ? parsed : []
+}
+
+async function loadSessionDetails() {
+  return axios.post(`${API_ENDPOINT}/session`, {
+    ssoToken: 'eyJrZXkiOiJtNyIsImFsZyI6ImRpciIsImVuYyI6IkExMjhDQkMtSFMyNTYifQ..711C_BSuYBi2EvgnQQoP4A.5D59RTyNQlK-I1Ucen0R4F1KCvRE8TgtxnFuasfrEdlXaLbibOmBDpk1B2wbRpm8ta36ARsB5bzCTOKAlJygMMJupnMPg1oJ-ff3bPOVXHuZXeAfNpgriHNT9ScQQZhFCBy9bZtHA49CPWy-_rE28Q3_HHCcLM9rzy5McLrsPbikI7JiPw5hB-eyQeN8X2OXl-f9vY5B2ltSTLTc2rJO32LMUaQ9aK1JXSnh2M5xN-mh0bcekNhIsy5_DtmGI7uPVa_dRKYO2KeklW4UwBe5R3idNvXoSb2NRifAW1E8y8W__HmAjzA7jNeTll4fyAI-mPx6f8zUqAj_XN628Oy9JCalD5SjPMXbKnj8_Xr2UdG4TR7CvyuvYgD212MnnfsyI3vWe9hqkv9nbeyu1bArrBd8mPpvKv2P8A8Ovkkpj4P1X1XTk2tKzWEyKMmD1AyQq3NvWLRgVX1gnLziyd6YCqZbBJBq_PYEZZAstH1_T0k5641n_XM8PedRExyhIjy1.3XqcDEyz4jsCAKUIou2OtA',
+    osVersion: 'Windows 10',
+    deviceModel: 'Chrome',
+    deviceType: 'PC',
+    deviceSerial: 'w112c2a90-da0a-11ef-b249-ad4a0ed7b0a7',
+    deviceOem: 'Chrome',
+    devicePrettyName: 'Chrome 131.0.0.0',
+    appVersion: '12.0',
+    language: 'en_US',
+    brand: 'fsro',
+    memberId: '0',
+    featureLevel: 6,
+    provisionData: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzc2OTIyNDgsImJyIjoiZnNybyIsImljIjp0cnVlLCJ1cCI6Im03Y3oiLCJvcCI6IjExNSIsImRlIjoiYnJhbmRNYXBwaW5nIiwiZHMiOiJ3MTEyYzJhOTAtZGEwYS0xMWVmLWIyNDktYWQ0YTBlZDdiMGE3In0.qFa51xG4wb2lYISB1OeweQoA00Edot4B9zQLjEStiuo'
+  }).then(r => r.data).catch(console.log)
 }
