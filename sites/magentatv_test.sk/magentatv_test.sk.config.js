bf986d41c357e3c5b66b77864b5e7d56d4d36ff9
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-25 13:16:12 -0500

    Update magentatv_test.sk.config.js

--- sites/magentatv_test.sk/magentatv_test.sk.config.js
+++ sites/magentatv_test.sk/magentatv_test.sk.config.js
@@ -1,11 +1,36 @@
-const axios = require('axios')
-const dayjs = require('dayjs')
-
-const BASIC_TOKEN = 'N2JjNWNjMTEtYWFiNi00ZmRlLTk0MTQtOThmOGNkYmY2NGI0'
+const axios = require('axios');
+const dayjs = require('dayjs');
 
 let session;
 
-const API_ENDPOINT = 'https://skgo.magio.tv/v2/television'
+const API_ENDPOINT = 'https://skgo.magio.tv/v2/television';
+
+async function fetchTokens(url) {
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Referer: 'https://magiogo.sk/',
+        Origin: 'https://magiogo.sk',
+        Pragma: 'no-cache',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site'
+      }
+    });
+    if (response.status === 200) {
+      const data = response.data;
+      const accessToken = data.accessToken;
+      const refreshToken = data.refreshToken;
+      return { accessToken, refreshToken };
+    } else {
+      console.error(`Failed to retrieve tokens, status code: ${response.status}`);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching tokens:', error);
+    return null;
+  }
+}
 
 module.exports = {
   site: 'magentatv_test.sk',
@@ -14,28 +39,28 @@ module.exports = {
     async headers() {
       if (!session) {
         session = await loadSessionDetails();
-        if (!session || !session.access_token) return null;
+        if (!session || !session.accessToken) return null;
       }
 
       return {
-        Authorization: `Bearer ${session.access_token}`,
+        Authorization: `Bearer ${session.accessToken}`,
         Referer: 'https://magiogo.sk/',
         Origin: 'https://magiogo.sk'
-      }
+      };
     },
     cache: {
       ttl: 24 * 60 * 60 * 1000 // 1 day
     }
   },
   url({ channel, date }) {
-    return `${API_ENDPOINT}/epg?filter=channel.id=in=(${channel.site_id});startTime=ge=${date.format('YYYY-MM-DDTHH:mm:ss.000')};startTime=le=${date.add(1, 'days').subtract(1, 's').format('YYYY-MM-DDTHH:mm:ss.000')}&lang=SK`
+    return `${API_ENDPOINT}/epg?filter=channel.id=in=(${channel.site_id});startTime=ge=${date.format('YYYY-MM-DDTHH:mm:ss.000')};startTime=le=${date.add(1, 'days').subtract(1, 's').format('YYYY-MM-DDTHH:mm:ss.000')}&lang=SK`;
   },
   parser({ content }) {
     try {
-      const programs = []
-      const data = JSON.parse(content)
+      const programs = [];
+      const data = JSON.parse(content);
       for (const channelItem of data.items) {
-        const channelId = channelItem.channel.channelId
+        const channelId = channelItem.channel.channelId;
         if (Array.isArray(channelItem.programs)) {
           channelItem.programs.forEach(item => {
             programs.push({
@@ -49,22 +74,22 @@ module.exports = {
               directors: item.program.programRole.directors.map(director => director.fullName).join(', '),
               start: dayjs(item.startTime),
               stop: dayjs(item.endTime)
-            })
-          })
+            });
+          });
         }
       }
 
-      return programs
+      return programs;
     } catch (error) {
-      console.error('Error parsing content:', error)
-      return []
+      console.error('Error parsing content:', error);
+      return [];
     }
   },
   async channels() {
-    if (!session || !session.access_token) {
+    if (!session || !session.accessToken) {
       session = await loadSessionDetails();
-      if (!session || !session.access_token) {
-        console.error('Error: Unable to retrieve session or access_token.');
+      if (!session || !session.accessToken) {
+        console.error('Error: Unable to retrieve session or accessToken.');
         return [];
       }
     }
@@ -72,42 +97,31 @@ module.exports = {
     const data = await axios
       .get(`${API_ENDPOINT}/channels?list=LIVE&queryScope=LIVE`, {
         headers: {
-          Authorization: `Bearer ${session.access_token}`,
+          Authorization: `Bearer ${session.accessToken}`,
           Referer: 'https://magiogo.sk/',
           Origin: 'https://magiogo.sk'
         }
       })
       .then(r => r.data)
-      .catch(console.log)
+      .catch(console.log);
 
     return data.items.map(item => {
       return {
         lang: 'sk',
         site_id: item.channel.channelId,
         name: item.channel.name
-      }
-    })
+      };
+    });
   }
-}
+};
 
 async function loadSessionDetails() {
-  return axios
-    .post('https://skgo.magio.tv/v2/auth/init', {}, {
-      params: {
-        refreshToken: BASIC_TOKEN
-      },
-      headers: {
-        Referer: 'https://magiogo.sk/',
-        Origin: 'https://magiogo.sk',
-        Pragma: 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'cross-site'
-      }
-    })
-    .then(r => r.data)
-    .catch(error => {
-      console.error('Error loading session details:', error)
-      return null
-    })
+  const url = 'https://skgo.magio.tv/v2/auth/init?dsid=Netscape.1737828170898.0.49497112051955927&deviceName=Web%20Browser&deviceType=OTT_WIN&osVersion=0.0.0&appVersion=4.0.21-hf.0&language=SK';
+  const tokens = await fetchTokens(url);
+  if (tokens) {
+    return tokens;
+  } else {
+    console.error('Error loading session details.');
+    return null;
+  }
 }
