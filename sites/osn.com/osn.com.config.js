823d6d4143d4ee0d7b653e13a45b7e26e321c2ed
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-01-29 16:05:13 -0500

    Update osn.com.config.js

--- sites/osn.com/osn.com.config.js
+++ sites/osn.com/osn.com.config.js
@@ -91,6 +91,7 @@ async function loadProgramDetails(item) {
   const url = `${API_ENDPOINT}/GetProgramDetails?prgmEPGUNIQID=${item.EPGUNIQID}&countryCode=SA`
   const data = await axios
     .get(url, { headers: {
+	'X-Requested-With': 'XMLHttpRequest',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
       } })
     .then(r => r.data)
