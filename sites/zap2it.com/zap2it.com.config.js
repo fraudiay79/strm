34b611e1c180f3f656400cb3b52f5e927038d27f
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-02-03 15:09:08 -0500

    Update zap2it.com.config.js

--- sites/zap2it.com/zap2it.com.config.js
+++ sites/zap2it.com/zap2it.com.config.js
@@ -2,32 +2,21 @@ const dayjs = require('dayjs')
 
 module.exports = {
   site: 'zap2it.com',
-  days: 13,
+  days: 2,
   request: {
     method: 'POST',
     headers: {
-  "accept": "*/*",
-  "accept-language": "en-US,en;q=0.9",
-  "cache-control": "no-cache",
-  "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-  "pragma": "no-cache",
-  "priority": "u=1, i",
-  "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\", \"Google Chrome\";v=\"132\"",
-  "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": "\"Windows\"",
-  "sec-fetch-dest": "empty",
-  "sec-fetch-mode": "cors",
-  "sec-fetch-site": "same-origin",
-  "x-requested-with": "XMLHttpRequest",
-  "cookie": "OTGPPConsent=DBABBg~BUoAAAKA.QA; usprivacy=1YNN; G_ENABLED_IDPS=google; OneTrustWPCCPAGoogleOptOut=false; _gid=GA1.2.651670712.1738607806; _gat_gtag_UA_34133884_39=1; _ga_K1JQ1EGMN2=GS1.1.1738607806.2.1.1738609030.0.0.0; _ga=GA1.1.910964067.1736261854; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Feb+03+2025+13%3A57%3A10+GMT-0500+(Eastern+Standard+Time)&version=202411.2.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=a74a4c6c-efb5-4896-9541-3a7c6049923c&interactionCount=1&isAnonUser=1&landingPath=NotLandingPage&GPPCookiesCount=1&groups=C0001%3A1%2CSPD_BG%3A1%2CC0002%3A1%2CC0004%3A1%2CC0003%3A1&AwaitingReconsent=false",
-  "Referer": "https://tvlistings.zap2it.com/ss-list.html?aid=gapzap",
-  "Referrer-Policy": "strict-origin-when-cross-origin"
-}
-
+	  'Accept': '*/*',
+	  'Accept-Encoding': 'gzip, deflate, br, zstd',
+	  'Origin': 'https://tvlistings.zap2it.com',
+	  'Referer': 'https://tvlistings.zap2it.com/ss-list.html?aid=gapzap',
+	  'X-Requested-With': 'XMLHttpRequest',
+	  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+    }
   },
   url: function ({ date, channel }) {
     const todayEpoch = Math.floor(date.startOf('day').utc().valueOf() / 1000)
-    return `https://tvlistings.zap2it.com/api/sslgrid?IsSSLinkNavigation=true&timespan=336&timestamp=${todayEpoch}&prgsvcid=${channel.site_id}&headendId=DITV&countryCode=USA&postalCode=32825&device=X&userId=-&aid=gapzap&DSTUTCOffset=-240&STDUTCOffset=-300&DSTStart=2025-03-09T02%3A00Z&DSTEnd=2025-11-02T02%3A00Z&languagecode=en-us`
+    return `https://tvlistings.zap2it.com/api/sslgrid?IsSSLinkNavigation=true&timespan=336&timestamp=${todayEpoch}&prgsvcid=${channel.site_id}&headendId=DITV&countryCode=USA&postalCode=32825&device=X&userId=-&aid=gapzap&DSTUTCOffset=-240&STDUTCOffset=-300&languagecode=en-us`
   },
   parser({ content, channel }) {
     let programs = []
