f3166546a5fffecd2617ee484664c5bc860addcd
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-04-22 16:38:38 -0400

    Update polska.py

--- platforms/polska.py
+++ platforms/polska.py
@@ -5,17 +5,32 @@ import os
 urls = [
     "https://vod.tvp.pl/api/products/399697/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
     "https://vod.tvp.pl/api/products/399698/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
-    "https://vod.tvp.pl/api/products/399702/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER,"
+    "https://vod.tvp.pl/api/products/399702/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
     "https://vod.tvp.pl/api/products/399703/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
     "https://vod.tvp.pl/api/products/399701/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399700/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399729/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399721/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399728/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/1998766/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399704/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399727/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399726/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399725/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399724/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399723/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER",
+    "https://vod.tvp.pl/api/products/399722/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER"
 ]
+
+# Corresponding names for the output files
+names = ["tvp1", "tvp2", "tvpsport", "tvphistoria", "tvpkobieta", "tvpkultura", "belsat", "tvpdocument", tvpkultura2", tvpnadobre", "tvpabc", "tvpabc2", "alfatvp", "tvphistoria", "tvprozrywka", "tvppolonia", "tvpnauka" ]
 
 # Directory to save output files
 output_dir = "links"
 os.makedirs(output_dir, exist_ok=True)
 
-# Loop through each URL
-for idx, url in enumerate(urls, start=1):
+# Loop through URLs and names
+for url, name in zip(urls, names):
     try:
         # Fetch JSON data from the API
         response = requests.get(url)
@@ -36,7 +51,7 @@ for idx, url in enumerate(urls, start=1):
         continue
 
     # Generate output file name
-    output_file = os.path.join(output_dir, f"tvp_{idx}.m3u8")
+    output_file = os.path.join(output_dir, f"{name}.m3u8")
 
     # Write the m3u8 link into the new file
     try:
