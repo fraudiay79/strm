ffbfc13879347a01344bb87e0e0139959276f5dc
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-05-06 16:15:01 -0400

    Update tvipt.sh

--- platforms/tvipt.sh
+++ platforms/tvipt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-file1="links/cnnpt.m3u8"
-file2="links/tvi.m3u8"
-file3="links/tviint.m3u8"
+file1="links/pt/cnnpt.m3u8"
+file2="links/pt/tvi.m3u8"
+file3="links/pt/tviint.m3u8"
 
 for file in "$file1" "$file2" "$file3"; do
   sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "$file"
