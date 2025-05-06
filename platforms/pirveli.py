9eff3501822de1a5afdb0b57741f0a2da2ab206c
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-05-06 13:06:03 -0400

    Update pirveli.py

--- platforms/pirveli.py
+++ platforms/pirveli.py
@@ -12,7 +12,7 @@ url = "https://api.myvideo.ge/api/v1/channel/chunk/pirvelitv"
 headers = {
     "accept": "application/json, text/plain, */*",
     "accept-language": "ge",
-    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjYwMTFkY2ZmYzFhNzM0NWE5YjU4Y2QwZmE1NTZhZjE5OTcyOTU3ODUzOTUyZDc0OWZkNTI2MmFlZWZiOWI5OGI1NDc1NTAyZWMzYThmYzhhIn0.eyJhdWQiOiI3IiwianRpIjoiNjAxMWRjZmZjMWE3MzQ1YTliNThjZDBmYTU1NmFmMTk5NzI5NTc4NTM5NTJkNzQ5ZmQ1MjYyYWVlZmI5Yjk4YjU0NzU1MDJlYzNhOGZjOGEiLCJpYXQiOjE3NDY1NDcwOTgsIm5iZiI6MTc0NjU0NzA5OCwiZXhwIjoxNzQ2NjMzNDk4LCJzdWIiOiIiLCJzY29wZXMiOltdfQ.kcwSi_5_jxcs2p75pZ0lUjurhKpngKiF01jmStKkmqvusXAjvR8JsZfp199DcyfEqpeP7i6PO2xyBecxijSALtViVxEdomWjbXMikme6ogro0aoCIXDsg3jrLoSCqoOH1u1jGSsjqEboQpsHaBYtB9AM2qO1hw-uerD0mmcZsGv8XGkovh8mHUJWZRiqOqYie2t85ZVvbSpq4DtoZhjTvhE6A9Cm1HKqOMBc5XgNsbT-OzcVLi8sngmn94gxJ0JODsc9AiyKytyQPss-pexqkTi7PTjkMhEzkXsINkih5cGoEBMFsQrFnPflWxkB_3XdS3cQc2F2xEX2GgOwEoEcg50y8NudVE2SSajTfNadr41VjJm4rXyjTYPtIMridPJeYWSsOjVeztuJCHf1xXSjOjHH1ACngjFGMr6x0ec5XG0gJaZc_ESwiuhKsAbsp0DZVifnRnYV2KJ5R3LmXxA3D_7mvJIKqS4N6TEcXq_Nhzd1_LS-vyq4ZMpgT7wZpBsbeaCdmMMOlfQuXvUghvnwFz5efNJHFjKtS8eAacZJlmfCGbV1LAyNtci3xPwcXvPkb7QEDEm5hxnoi1dVXR2TMpuXmiNdupJoWAWA1w2JcGLM0e5s-yJ-qz2YlXsBVT0MoK883Suf2KZIPiMFWgT_K56T7du3SFc_6LqcPe7KVRo",
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjYwMTFkY2ZmYzFhNzM0NWE5YjU4Y2QwZmE1NTZhZjE5OTcyOTU3ODUzOTUyZDc0OWZkNTI2MmFlZWZiOWI5OGI1NDc1NTAyZWMzYThmYzhhIn0.eyJhdWQiOiI3IiwianRpIjoiNjAxMWRjZmZjMWE3MzQ1YTliNThjZDBmYTU1NmFmMTk5NzI5NTc4NTM5NTJkNzQ5ZmQ1MjYyYWVlZmI5Yjk4YjU0NzU1MDJlYzNhOGZjOGEiLCJpYXQiOjE3NDY1NDcwOTgsIm5iZiI6MTc0NjU0NzA5OCwiZXhwIjoxNzQ2NjMzNDk4LCJzdWIiOiIiLCJzY29wZXMiOltdfQ.kcwSi_5_jxcs2p75pZ0lUjurhKpngKiF01jmStKkmqvusXAjvR8JsZfp199DcyfEqpeP7i6PO2xyBecxijSALtViVxEdomWjbXMikme6ogro0aoCIXDsg3jrLoSCqoOH1u1jGSsjqEboQpsHaBYtB9AM2qO1hw-uerD0mmcZsGv8XGkovh8mHUJWZRiqOqYie2t85ZVvbSpq4DtoZhjTvhE6A9Cm1HKqOMBc5XgNsbT-OzcVLi8sngmn94gxJ0JODsc9AiyKytyQPss-pexqkTi7PTjkMhEzkXsINkih5cGoEBMFsQrFnPflWxkB_3XdS3cQc2F2xEX2GgOwEoEcg50y8NudVE2SSajTfNadr41VjJm4rXyjTYPtIMridPJeYWSsOjVeztuJCHf1xXSjOjHH1ACngjFGMr6x0ec5XG0gJaZc_ESwiuhKsAbsp0DZVifnRnYV2KJ5R3LmXxA3D_7mvJIKqS4N6TEcXq_Nhzd1_LS-vyq4ZMpgT7wZpBsbeaCdmMMOlfQuXvUghvnwFz5efNJHFjKtS8eAacZJlmfCGbV1LAyNtci3xPwcXvPkb7QEDEm5hxnoi1dVXR2TMpuXmiNdupJoWAWA1w2JcGLM0e5s-yJ-qz2YlXsBVT0MoK883Suf2KZIPiMFWgT_K56T7du3SFc_6LqcPe7KVRo,
     "priority": "u=1, i",
     "sec-ch-ua": "\"Chromium\";v=\"136\", \"Google Chrome\";v=\"136\", \"Not.A/Brand\";v=\"99\"",
     "sec-ch-ua-mobile": "?0",
@@ -41,11 +41,20 @@ if response.status_code == 200:
             content_response = requests.get(file_url, headers=headers)
             if content_response.status_code == 200:
                 content = content_response.text
-                
-                # Save the unmodified content to a file
+                lines = content.split("\n")
+                modified_content = ""
+
+                for line in lines:
+                    if not line.startswith("#") and not line.startswith("http"):
+                        full_url = base_url + line
+                        modified_content += full_url + "\n"
+                    else:
+                        modified_content += line + "\n"
+
+                # Save the modified content to a file
                 output_file = os.path.join(output_dir, "stream_links.m3u8")
                 with open(output_file, "w") as file:
-                    file.write(content)
+                    file.write(modified_content)
 
                 print(f"Created file: {output_file}")
 
