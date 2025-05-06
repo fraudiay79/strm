7fc1a7a877a2c008ac5c0f26dafc92ac46365607
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-05-06 13:03:08 -0400

    Update pirveli.py

--- platforms/pirveli.py
+++ platforms/pirveli.py
@@ -41,20 +41,11 @@ if response.status_code == 200:
             content_response = requests.get(file_url, headers=headers)
             if content_response.status_code == 200:
                 content = content_response.text
-                lines = content.split("\n")
-                modified_content = ""
-
-                for line in lines:
-                    if line.startswith("live_"):  # Replace relative paths with full URLs
-                        full_url = base_url + line
-                        modified_content += full_url + "\n"
-                    else:
-                        modified_content += line + "\n"
-
-                # Save the modified content to a file
+                
+                # Save the unmodified content to a file
                 output_file = os.path.join(output_dir, "stream_links.m3u8")
                 with open(output_file, "w") as file:
-                    file.write(modified_content)
+                    file.write(content)
 
                 print(f"Created file: {output_file}")
 
