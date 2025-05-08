32ab443c8a9b22d6fc7a5fb145ab74f16f87e4dd
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-05-08 16:39:41 -0400

    Update alrai.py

--- platforms/alrai.py
+++ platforms/alrai.py
@@ -1,39 +1,21 @@
 import requests
 import re
 
-base_url = "https://live.kwikmotion.com/alraimedialive/alraitv.smil/"
-url = "https://www.alraimedia.com/livestream/"
-response = requests.get(url)
+url = 'https://www.alraimedia.com/livestream/'
+
+response = requests.get(url, verify=False)
 
 if response.status_code == 200:
-    site_content = response.text
-
-    match = re.search(r'https://live.kwikmotion.com/alraimedialive/alraitv.smil/.*?\.m3u8\?hdnts=[^"]+', site_content)
-
+    match = re.search(r"https://live.kwikmotion.com/alraimedialive/alraitv.smil/.*?\.m3u8\?hdnts=[^\"]+", response.text)
+  
     if match:
-        m3u8_url = match.group(0)
-        #print(f"Extracted M3U8 URL: {m3u8_url}")
-
-        # Fetch m3u8 content
-        content_response = requests.get(m3u8_url)
-        
-        if content_response.status_code == 200:
-            content = content_response.text
-            lines = content.split("\n")
-            modified_content = ""
-
-            for line in lines:
-                line = line.strip()
-                if line.startswith("alraitvpublish"):
-                    full_url = base_url + line
-                    modified_content += full_url + "\n"
-                else:
-                    modified_content += line + "\n"
-
-            print(modified_content)
-        else:
-            print("Failed to fetch m3u8 content.")
+        erstrm = match.group(0)  # Using group(0) to extract the full match
+        # Print output in the specified format
+        print("#EXTM3U")
+        print("#EXT-X-VERSION:3")
+        print("#EXT-X-STREAM-INF:BANDWIDTH=2126865,FRAME-RATE=25,RESOLUTION=1920x1080,CODECS="avc1.4d0028,mp4a.40.2",CLOSED-CAPTIONS=NONE")
+        print(erstrm)
     else:
-        print("Live stream URL not found in the page content.")
+        print("erstrm not found in the content.")
 else:
-    print("Failed to fetch the website content.")
+    print(f"Failed to fetch content. HTTP Status code: {response.status_code}")
