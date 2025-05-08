6fd68f42db6b0a3a0b0d6d4809a265e85b7e3d7e
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-05-08 16:25:28 -0400

    Update nowtvtr.py

--- platforms/nowtvtr.py
+++ platforms/nowtvtr.py
@@ -9,9 +9,31 @@ if response.status_code == 200:
     match = re.search(r"daiUrl\s*:\s*'(https?://[^\']+)'", response.text)
   
     if match:
-        erstrm = match.group(1)
-        print(erstrm)
+        m3u8_url = match.group(1)  # Extracted M3U8 URL
+        # print(f"Extracted M3U8 URL: {m3u8_url}")
+
+        # Fetch m3u8 content
+        content_response = requests.get(m3u8_url)
+
+        if content_response.status_code == 200:
+            content = content_response.text
+            lines = content.split("\n")
+            modified_content = ""
+
+            base_url = m3u8_url.rsplit('/', 1)[0]  # Extract the base URL
+
+            for line in lines:
+                line = line.strip()
+                if line.startswith("nowtv"):
+                    full_url = f"{base_url}/{line}"
+                    modified_content += full_url + "\n"
+                else:
+                    modified_content += line + "\n"
+
+            print(modified_content)
+        else:
+            print("Failed to fetch m3u8 content.")
     else:
-        print("erstrm not found in the content.")
+        print("Live stream URL not found in the page content.")
 else:
     print(f"Failed to fetch content. HTTP Status code: {response.status_code}")
