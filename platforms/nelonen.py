dd1011560917221ecf29089f0a9f7c27c0db2e24
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-06-23 15:09:47 -0400

    Update nelonen.py

--- platforms/nelonen.py
+++ platforms/nelonen.py
@@ -9,7 +9,7 @@ print('#EXT-X-VERSION:3')
 print('#EXT-X-INDEPENDENT-SEGMENTS')
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
 }
 s = requests.Session()
 
