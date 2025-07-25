d503d0493988dd424dfb2fabea8c172e030121a7
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-07-25 18:43:47 -0400

    Update mi.tv.config.js

--- sites/mi.tv/mi.tv.config.js
+++ sites/mi.tv/mi.tv.config.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const cheerio = require('cheerio')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
@@ -6,12 +7,20 @@ const customParseFormat = require('dayjs/plugin/customParseFormat')
 dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 
+const customAxios = axios.create({
+  headers: {
+    'Accept': 'text/html,application/json;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/138 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+})
+
 module.exports = {
   site: 'mi.tv',
   days: 2,
   url({ date, channel }) {
     const [country, id] = channel.site_id.split('#')
-
     return `https://mi.tv/${country}/async/channel/${id}/${date.format('YYYY-MM-DD')}/0`
   },
   parser({ content, date }) {
@@ -22,11 +31,11 @@ module.exports = {
       const $item = cheerio.load(item)
       let start = parseStart($item, date)
       if (!start) return
-      if (prev) {
-        if (start.isBefore(prev.start)) {
-          start = start.add(1, 'd')
-          date = date.add(1, 'd')
-        }
+      if (prev && start.isBefore(prev.start)) {
+        start = start.add(1, 'd')
+        date = date.add(1, 'd')
+        prev.stop = start
+      } else if (prev) {
         prev.stop = start
       }
       const stop = start.add(1, 'h')
@@ -39,69 +48,31 @@ module.exports = {
         stop
       })
     })
-
     return programs
   },
   async channels({ country }) {
-    let lang = 'es'
-    if (country === 'br') lang = 'pt'
+    let lang = country === 'br' ? 'pt' : 'es'
+    const url = `https://mi.tv/${country}/sitemap`
 
-    const axios = require('axios')
-    const data = await axios
-      .get(`https://mi.tv/${country}/sitemap`)
+    const data = await customAxios
+      .get(url)
       .then(r => r.data)
       .catch(console.log)
 
     const $ = cheerio.load(data)
+    const channels = []
 
-    let channels = []
-    $(`#page-contents a[href*="${country}/canales"], a[href*="${country}/canais"]`).each(
-      (i, el) => {
-        const name = $(el).text()
-        const url = $(el).attr('href')
-        const [, , , channelId] = url.split('/')
-
-        channels.push({
-          lang,
-          name,
-          site_id: `${country}#${channelId}`
-        })
-      }
-    )
+    $(`#page-contents a[href*="${country}/canales"], a[href*="${country}/canais"]`).each((i, el) => {
+      const name = $(el).text().trim()
+      const url = $(el).attr('href')
+      const [, , , channelId] = url.split('/')
+      channels.push({
+        lang,
+        name,
+        site_id: `${country}#${channelId}`
+      })
+    })
 
     return channels
   }
-}
-
-function parseStart($item, date) {
-  const timeString = $item('a > div.content > span.time').text()
-  if (!timeString) return null
-  const dateString = `${date.format('MM/DD/YYYY')} ${timeString}`
-
-  return dayjs.utc(dateString, 'MM/DD/YYYY HH:mm')
-}
-
-function parseTitle($item) {
-  return $item('a > div.content > h2').text().trim()
-}
-
-function parseCategory($item) {
-  return $item('a > div.content > span.sub-title').text().trim()
-}
-
-function parseDescription($item) {
-  return $item('a > div.content > p.synopsis').text().trim()
-}
-
-function parseImage($item) {
-  const backgroundImage = $item('a > div.image-parent > div.image').css('background-image')
-  const [, image] = backgroundImage.match(/url\('(.*)'\)/) || [null, null]
-
-  return image
-}
-
-function parseItems(content) {
-  const $ = cheerio.load(content)
-
-  return $('#listings > ul > li').toArray()
 }
