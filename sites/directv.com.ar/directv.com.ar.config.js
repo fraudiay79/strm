e001e59e4e7a2580d7cfc8b152633025e9425e7f
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-07-25 16:49:11 -0400

    Update directv.com.ar.config.js

--- sites/directv.com.ar/directv.com.ar.config.js
+++ sites/directv.com.ar/directv.com.ar.config.js
@@ -26,10 +26,11 @@ module.exports = {
       'Sec-Fetch-Mode': 'cors',
       'Sec-Fetch-Site': 'same-origin',
       'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
       'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
       'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"'
+      'sec-ch-ua-platform': '"Windows"',
+      "X-Requested-With": "XMLHttpRequest"
     },
     data({ channel, date }) {
       const [channelNum, channelName] = channel.site_id.split('#')
