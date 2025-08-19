8270c07027fb049ab9b193618af01d625039c02e
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-08-19 13:03:08 -0400

    Update galamtv.kz.config.js

--- sites/galamtv.kz/galamtv.kz.config.js
+++ sites/galamtv.kz/galamtv.kz.config.js
@@ -22,26 +22,33 @@ module.exports = {
         }
     },
     url({ channel, date }) {
-        const todayEpoch = date.startOf('day').unix()
-        const nextDayEpoch = date.add(1, 'day').startOf('day').unix()
-        return `https://galam.server-api.lfstrm.tv/channels/${channel.site_id}/programs?period=${todayEpoch}:${nextDayEpoch}`
+        return `https://galam.server-api.lfstrm.tv/epg/v2/schedules/${channel.site_id}/spread?centralPageId=${date.format('YYYY-MM-DD')}t12d12h`
     },
     parser: function({ content }) {
         let programs = []
         const data = JSON.parse(content)
-        const programsData = data.programs || []
+        
+        // Check if pagesWithEvents exists and has data
+        if (!data.pagesWithEvents || !Array.isArray(data.pagesWithEvents)) {
+            return programs
+        }
 
-        programsData.forEach(program => {
-            const start = dayjs.unix(program.scheduleInfo.start)
-            const stop = dayjs.unix(program.scheduleInfo.end)
+        // Process each page with events
+        data.pagesWithEvents.forEach(page => {
+            if (page.events && Array.isArray(page.events)) {
+                page.events.forEach(event => {
+                    const start = dayjs(event.scheduledFor.begin)
+                    const stop = dayjs(event.scheduledFor.end)
 
-            programs.push({
-                title: program.metaInfo.title,
-                description: program.metaInfo.description,
-                icon: program.mediaInfo.thumbnails[0].url,
-                start,
-                stop
-            })
+                    programs.push({
+                        title: event.title,
+                        description: event.eventDescriptionMedium,
+                        icon: event.logoUrl,
+                        start,
+                        stop
+                    })
+                })
+            }
         })
 
         return programs
