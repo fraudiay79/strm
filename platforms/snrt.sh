469bfd356fffadaf61f08302c5cefacd33d969f4
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2025-09-23 12:40:02 -0400

    Update snrt.sh

--- platforms/snrt.sh
+++ platforms/snrt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Define arrays for files and their corresponding token URLs
-files=("links/ma/aloula.m3u8" "links/ma/almaghribia.m3u8" "links/ma/arryadia.m3u8" "links/ma/assadissa.m3u8" "links/ma/athaqafia.m3u8" "links/ma/laayoune.m3u8" "links/ma/tamazight.m3u8")
+files=("links/ma/aloula.m3u8" "links/ma/almaghribia.m3u8" "links/ma/arryadia.m3u8"  "links/ma/arryadiatnt.m3u8" "links/ma/assadissa.m3u8" "links/ma/athaqafia.m3u8" "links/ma/laayoune.m3u8" "links/ma/tamazight.m3u8")
 urls=("https://cdn.live.easybroadcast.io/abr_corp/73_aloula_w1dqfwm/playlist_dvr.m3u8"
       "https://cdn.live.easybroadcast.io/abr_corp/73_almaghribia_83tz85q/playlist_dvr.m3u8"
       "https://cdn.live.easybroadcast.io/abr_corp/73_arryadia_k2tgcj0/playlist_dvr.m3u8"
+      "https://cdn.live.easybroadcast.io/abr_corp/73_arryadia-tnt_zcmwjdc/playlist_dvr.m3u8"
       "https://cdn.live.easybroadcast.io/abr_corp/73_assadissa_7b7u5n1/playlist_dvr.m3u8"
       "https://cdn.live.easybroadcast.io/abr_corp/73_arrabia_hthcj4p/playlist_dvr.m3u8"
       "https://cdn.live.easybroadcast.io/abr_corp/73_laayoune_pgagr52/playlist_dvr.m3u8"
