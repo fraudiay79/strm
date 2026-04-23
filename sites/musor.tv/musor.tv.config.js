9e599b0f8f366db8742f0730f592ed2a96b8c641
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2026-04-23 15:17:14 -0400

    Update musor.tv.config.js

--- sites/musor.tv/musor.tv.config.js
+++ sites/musor.tv/musor.tv.config.js
@@ -3,13 +3,14 @@ const axios = require('axios')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
+const uniqBy = require('lodash.uniqby')
 
 dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 
 const headers = {
   'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0'
 }
 
 module.exports = {
@@ -30,9 +31,11 @@ module.exports = {
       let start = parseStart($item)
       if (prev) prev.stop = start
       const stop = start.add(30, 'm')
+      const details = parseDetails($item)
       programs.push({
         title: parseTitle($item),
-        description: parseDescription($item),
+        subTitle: details.subTitle,
+        description: details.description,
         image: parseImage($item),
         start,
         stop
@@ -49,38 +52,54 @@ module.exports = {
 
     const $ = cheerio.load(html)
     const channels = $('body > div.big_content > div > nav > table > tbody > tr > td > a').toArray()
-    return channels
-      .map(item => {
-        const $item = $(item)
-        const url = $item.attr('href')
-        if (!url.startsWith('//musor.tv/mai/tvmusor/')) return null
-        const site_id = url.replace('//musor.tv/mai/tvmusor/', '')
-        return {
-          lang: 'hu',
-          site_id,
-          name: $item.text()
-        }
-      })
-      .filter(i => i)
+
+    return uniqBy(
+      channels
+        .map(item => {
+          const $item = $(item)
+          const url = $item.attr('href')
+          if (!url.startsWith('//musor.tv/mai/tvmusor/')) return null
+          const site_id = url.replace('//musor.tv/mai/tvmusor/', '')
+          return {
+            lang: 'hu',
+            site_id,
+            name: $item.text()
+          }
+        })
+        .filter(i => i),
+      'site_id'
+    )
   }
 }
 
 function parseImage($item) {
-  const imgSrc = $item.find('div.smartpe_screenshot > img').attr('src')
+  const imgSrc = $item.find('div.progentry_screenshot > img').attr('src')
 
   return imgSrc ? `https:${imgSrc}` : null
 }
 
 function parseTitle($item) {
-  return $item.find('div:nth-child(2) > div > h3 > a').text().trim()
+  return $item.find('h3 > a').text().trim()
 }
 
-function parseDescription($item) {
-  return $item.find('div:nth-child(5) > div > div').text().trim()
+function parseDetails($item) {
+  const details = $item.find('div.progentrylong').html().split('<br>').filter(Boolean)
+
+  if (details.length === 1)
+    return {
+      description: details[0]
+    }
+
+  const [subTitle, description] = details
+
+  return {
+    subTitle,
+    description
+  }
 }
 
 function parseStart($item) {
-  let datetime = $item.find('div:nth-child(1) > div > div > div > div > time').attr('content')
+  let datetime = $item.find('time').attr('content')
   if (!datetime) return null
 
   return dayjs.utc(datetime.replace('GMT', 'T'), 'YYYY-MM-DDTHH:mm:ss')
@@ -89,5 +108,5 @@ function parseStart($item) {
 function parseItems(content) {
   const $ = cheerio.load(content)
 
-  return [$, $('div.multicolumndayprogarea > div.smartpe_progentry').toArray()]
+  return [$, $('div.progarea > div.progentry > div.progentry_internal').toArray()]
 }
