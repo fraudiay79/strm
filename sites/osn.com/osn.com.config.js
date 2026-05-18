ef467b4e8525a0049e6e663c5ddea7efb16ea7b5
fraudiay79 <60631277+fraudiay79@users.noreply.github.com>
2026-05-18 09:10:03 -0400

    Update osn.com.config.js

--- sites/osn.com/osn.com.config.js
+++ sites/osn.com/osn.com.config.js
@@ -1,101 +1,139 @@
+const axios = require('axios')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone')
-const axios = require('axios')
-
-const API_ENDPOINT = 'https://www.osn.com/api/TVScheduleWebService.asmx'
+const crypto = require('crypto')
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-const packages = { 'OSNTV CONNECT': 3720, 'OSNTV PRIME': 3733, 'ALFA': 1281, 'OSN PINOY PLUS EXTRA': 3519 }
-const country = 'SA'
-const tz = 'Asia/Riyadh'
+// https://www.osn.com/_next/static/chunks/87cf1e375968671c.js
+
+const cipherName = 'aes-256-cbc'
+const cipherKey = Buffer.from('65a04b9b5591f27c837fac433274b494403e0eec5b43698060c28e1162d4460f', 'hex')
+const cipherIv = Buffer.from('b7cc0a48d6d023bc1a2a670953ec5622', 'hex')
+
+const tz = dayjs.tz.guess()
+const oneDayMs = 864e5
+const headers = {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0'
+}
+let batch = 0
 
 module.exports = {
   site: 'osn.com',
   days: 2,
   url({ channel, date }) {
-    return `${API_ENDPOINT}/time?dt=${encodeURIComponent(date.format('MM/DD/YYYY'))}&co=${country}&ch=${channel.site_id}&mo=false&hr=0`
+    const data = getChannelData({ channel, date })
+    return `https://www.osn.com/apidata/tv-schedule-timeline?t=batch${++batch}-time${data.startTime}-${data.endTime}-boxAndroid`
   },
   request: {
-    headers({ channel }) {
+    headers({ channel, date }) {
       return {
-        Referer: `https://www.osn.com/${channel.lang}-${country.toLowerCase()}/watch/tv-schedule`
+        ...headers,
+        'X-Encrypted-Data': encrypt(JSON.stringify(getChannelData({ channel, date })))
       }
     }
   },
-  async parser({ content, channel, date }) {
-    let programs = []
-    if (!content) return programs
-
-    let items = JSON.parse(content)
-    if (!items.length) return programs
-	
-    for (let item of items) {
-      const title = channel.lang === 'ar' ? item.Arab_Title : item.Title
-      const start = dayjs.tz(item.StartDateTime, 'DD MMM YYYY, HH:mm', tz)
-      const duration = parseInt(item.TotalDivWidth / 4.8)
-      const stop = start.add(duration, 'm')
-      const detail = await loadProgramDetails(item)
-      programs.push({
-        title,
-        description: channel.lang === 'ar' ? detail.Arab_Synopsis : detail.Synopsis,
-        icon: detail.ProgramImage,
-        category: channel.lang === 'ar' ? detail.GenreArabicName : detail.GenreEnglishName,
-        subtitle: channel.lang === 'ar' ? detail.EpisodeAr : detail.EpisodeEn,
-        season: detail.SeasonNo,
-        episode: detail.EpisodeNo,
-        start,
-        stop
-      })
+  parser({ content, channel }) {
+    const programs = []
+    if (typeof content === 'string' || Buffer.isBuffer(content)) {
+      content = JSON.parse(content)
+    }
+    if (content?.encrypted) {
+      content = JSON.parse(decrypt(content.encrypted))
+    }
+    if (Array.isArray(content?.entries)) {
+      content.entries
+        .filter(entry => entry.guid == channel.site_id)
+        .forEach(entry => {
+          if (Array.isArray(entry.listings)) {
+            const listings = entry.listings
+              .map(listing => {
+                const res = {
+                  title: dotProp(listing, `program.titleLocalized.${channel.lang}`),
+                  subTitle: dotProp(listing, `program.osnprogram$seriesTitleLocalized.${channel.lang}`),
+                  description: dotProp(listing, `program.descriptionLocalized.${channel.lang}`),
+                  start: dayjs.tz(listing.startTime, tz),
+                  stop: dayjs.tz(listing.endTime, tz),
+                  categories: (dotProp(listing, 'program.tags') || [])
+                    .map(tag => dotProp(tag, `titleLocalized.${channel.lang}`)),
+                  icon: dotProp(listing, `program.thumbnails.landscapeTitleImage-${channel.lang}.url`),
+                  season: dotProp(listing, 'program.tvSeasonNumber'),
+                  episode: dotProp(listing, 'program.tvSeasonEpisodeNumber'),
+                  date: dotProp(listing, 'program.year')
+                }
+                if (res.subTitle) {
+                  const subTitle = res.title
+                  res.title = res.subTitle
+                  res.subTitle = subTitle
+                }
+                if (res.date) {
+                  res.date = `${res.date}`
+                }
+                return res
+              })
+            programs.push(...listings)
+          }
+        })
     }
 
     return programs
   },
   async channels({ lang = 'ar' }) {
-    const result = {}
-    for (const pkg of Object.values(packages)) {
-      let page = 1
-      let hasMorePages = true
+    const channels = []
+    const result = await axios
+      .get('https://www.osn.com/apidata/channels?platform=Android', { headers })
+      .then(response => response.data)
+      .catch(console.error)
 
-      while (hasMorePages) {
-        const channels = await axios
-          .get(`${API_ENDPOINT}/chnl?pg=${page}&pk=${pkg}&gn=0&cu=en-US&bx=2&dt=${encodeURIComponent(date.format('MM/DD/YYYY'))}`)
-          .then(response => response.data)
-          .catch(console.error)
-
-      if (Array.isArray(channels) && channels.length > 0) {
-        for (const ch of channels) {
-          if (result[ch.channelCode] === undefined) {
-            result[ch.channelCode] = {
-              lang,
-              site_id: ch.channelCode,
-              name: ch.channeltitle
-            }
-          }
-        }
-        page++
-      } else {
-        hasMorePages = false
-      }
+    if (result?.encrypted) {
+      const items = JSON.parse(decrypt(result.encrypted))
+      channels.push(...items.map(item => ({
+        lang,
+        site_id: item.guid,
+        name: item.title
+      })))
     }
-    }
-
-    return Object.values(result)
+    return channels
   }
 }
 
-async function loadProgramDetails(item) {
-  if (!item.EPGUNIQID) return {}
-  const url = `${API_ENDPOINT}/GetProgramDetails?prgmEPGUNIQID=${item.EPGUNIQID}&countryCode=SA`
-  const data = await axios
-    .get(url, { headers: {
-	'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
-      } })
-    .then(r => r.data)
-    .catch(console.log)
+function encrypt(data, encoding = 'utf8') {
+  const cipher = crypto.createCipheriv(cipherName, cipherKey, cipherIv)
+  const encrypted = cipher.update(data, encoding, 'base64')
+  return (encrypted + cipher.final('base64'))
+}
 
-  return data || {}
+function decrypt(data, encoding = 'utf8') {
+  const decipher = crypto.createDecipheriv(cipherName, cipherKey, cipherIv)
+  const decrypted = decipher.update(data, 'base64', encoding)
+  return (decrypted + decipher.final(encoding))
+}
+
+function getChannelData({ channel, date }) {
+  const startOfDay = date.tz(tz).startOf('d')
+  return {
+    channelGuid: channel.site_id,
+    startTime: startOfDay.valueOf(),
+    endTime: startOfDay.valueOf() + oneDayMs
+  }
+}
+
+function dotProp(o, prop) {
+  if (typeof o === 'object') {
+    const props = prop.split('.')
+    while (props.length) {
+      const k = props.shift()
+      if (o[k] !== undefined) {
+        o = o[k]
+      } else {
+        break
+      }
+    }
+    if (props.length === 0) {
+      return o
+    }
+  }
 }
